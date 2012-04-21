415c6d9af31ecc8592c5717da3495e46b6e5ba08
Luchs <Luchs@euirc.eu>
2012-04-21 22:46:21 +0200

    Make the Akku work
     - updates the MatSys
     - saves some arbitrary value without polling for more energy

--- Clinfinity.c4d/Structures.c4d/Akku.c4d/Script.c
+++ Clinfinity.c4d/Structures.c4d/Akku.c4d/Script.c
@@ -1,30 +1,17 @@
 /*-- Battery --*/
 
 #strict 2
-#include B_40
-#include PWRG // Ist Energie"produzent"
-#include PWRC // ...und Energie"verbraucher"
-#include DACT // Damagecontrol
-#include L_CA // Baugrafik
 
-// Es mangelt uns nicht an Energie
-private func FxEnergyNeedStart() {
-	return -1;
-}
+#include L_SS
 
-public func GetGeneratorPriority() {
-	return 1;
-}
+public func MaxFill() { return 1500; }
 
-public func IsBattery() {
-	return true;
-}
-
-local Capacity;
-
-func Initialize() {
-  SetCapacity(1500); 
-  return(1);
+protected func Initialize() {
+	// create dummy effect if this is the first Akku
+	// this ensures that when the last Akku gets destroyed, the display is reset to 0
+	if(!GetEffect("MatSysSTEM"))
+		AddEffect("MatSysSTEM", 0, 1, 0);
+	AddEffect("MatSysSTEM", 0, 1, 0, this);
 }
 
 public func Deconstruction() {
@@ -34,95 +21,34 @@ public func Deconstruction() {
 	return _inherited(...);
 }
 
-public func SetPower() {
-	_inherited(...);
+private func OnFillChange() {
 	UpdateDisplay();
-	return true;
-}
-
-public func DoPower() {
-	_inherited(...);
-	UpdateDisplay();
-	return true;
 }
 
 private func UpdateDisplay() {
-	SetGraphics(0, this, _BAR, 1, GFXOV_MODE_Action, Format("%d/5", GetPower() * 5 / GetCapacity()));
+	SetGraphics(0, this, _BAR, 1, GFXOV_MODE_Action, Format("%d/5", GetFill() * 5 / MaxFill()));
 	SetObjDrawTransform(1000, 0, -13500, 0, 1000, 1000, this, 1);
 	return 1;
 }
 
-// Get- Funktionen
-public func GetCapacity() {
-  return Capacity;
-}
-
-// Set- Funktionen
-public func SetCapacity(int NewCapacity) {
-  Capacity = NewCapacity;
-  UpdateDisplay();
-  return true;
-}
-
-func Act() { // Timercall
-	if(!(GetOCF() & OCF_Fullcon))
-		return;
-  // Energie saugen
-  var iEnergy = BoundBy(GetCapacity() - GetPower(), 0, 10);
-  if(iEnergy && CheckPower(iEnergy)) {
-  	DoPower(iEnergy);
-  }
-  return 1;
-}
-
-//Steuerung
+// Steuerung
   
 protected func ControlUp() {
-	Message("$TxtStored$", this, GetPower());
-}
-
-protected func ControlDig(object pClonk) {
-	var pObj = FindObject2(Find_Container(pClonk), Find_ID(CELL));
-	if(pObj) {
-		if(GetPower() >= 200) {
-			DoPower(-200);
-			pObj -> ChangeDef(FCLL);
-			Message("$TxtCharged$", this);
-		}
-		else
-			Message("$TxtNotEnoughPower$", this);
-	} else {
-		Message("$TxtNoCell$", this);
-	}
-	return 1;
-}
-
-protected func ControlThrow(object pClonk) {
-	var pObj = FindObject2(Find_Container(pClonk), Find_ID(FCLL));
-	if(pObj) {
-		if(GetPower() < GetCapacity()) {
-			DoPower(200);
-			pObj -> ChangeDef(CELL);
-			pObj -> Enter(pClonk);
-			Message("$TxtAssign$", this);
-		}
-	} else {
-		Message("$TxtNoChargedCell$", this);
-	}
-	return 1;
-}
-
-/* Erforschbar */
-
-public func MarsResearch() {
-	return true;
+	Message("$TxtStored$", this, GetFill());
 }
 
 public func MaxDamage() { return 25; } //Maximaler Schaden
 
 /* Anzeige-fix nach Spielstand laden */
 
-func UpdateTransferZone()
-{
+protected func UpdateTransferZone() {
 	UpdateDisplay();
 }
+
+/* Material System fill level */
+public func FxMatSysSTEMUpdate(object target, int effectNum, int plr) {
+	if(!this || Hostile(GetOwner(), plr))
+		return 0;
+	return GetFill();
+}
+
--- Clinfinity.c4d/System.c4g/MatSys.c
+++ Clinfinity.c4d/System.c4g/MatSys.c
@@ -30,7 +30,7 @@ global func MatSysDoFill(int iChange, int iPlr, id Key) {
 }
 
 global func GetMatSysIDs() {
-	var aIDs = [WBRL, WOOD, METL];
+	var aIDs = [WBRL, STEM, WOOD, METL];
 	ConcatArrays(aIDs, GameCall("SpecialMatSysIDs")); // vertauschte Reihenfolge, da von rechts nach links
 	return aIDs;
 }
