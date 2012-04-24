1fa5d82649536254d4e7c5f61dfff7efe9dd6e87
Nachtschatten <ChyH@gmx.net>
2012-04-24 15:32:19 +0200

    Empty effect, will be used for wing suit.

---
+++ Clinfinity.c4d/System.c4g/WingSuit.c
@@ -0,0 +1,33 @@
+#strict 2
+
+#appendto CLNK
+
+protected func ControlDownDouble() {
+	if( _inherited() == 0 ) {
+		if( GetAction() == "Jump" ) {
+			if(GetEffect("WingSuit", this) == 0) {
+				AddEffect("WingSuit", this, 150, 1, this);
+			}
+		}
+	}
+	return 0;
+}
+
+protected func FxWingSuitStart(object target, int effectNumber, int temporary) {
+	// WingSuitFlight-Action setzen
+}
+
+protected func FxWingSuitTimer(object target, int effectNumber, int effectTime) {
+	if(target->GetAction() == "WingSuitFlight") {
+		// Geschwindigkeit berechnen und setzen
+		return 0;
+	} else {
+		return -1;
+	}
+}
+
+protected func FxWingSuitStop(object target, int effectNumber, int reason, bool temporary) {
+	if(!temporary) {
+		// Wieder normale Jump-Action setzen, Geschwindigkeit eventuell auch setzen.
+	}
+}
