6ad50c8823353e5844f27bc43aad25421548fb25
Nachtschatten <ChyH@gmx.net>
2012-04-28 00:41:23 +0200

    Started implementing a control lever for platforms.

---
+++ Clinfinity.c4d/Vehicles.c4d/Platform.c4d/ControlLever.c4d/Script.c
@@ -0,0 +1,13 @@
+#strict 2
+
+protected func MouseSelection(int player) {
+	Sound("Ding");
+}
+
+protected func ControlUp(object controller) {
+	Sound("Ding");
+}
+
+protected func ControlDown(object controller) {
+	Sound("Ding");
+}
