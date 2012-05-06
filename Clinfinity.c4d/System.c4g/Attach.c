62efcca37f7dfbdab2a6b42c833ed11444229530
Luchs <Luchs@euirc.eu>
2012-05-06 23:30:50 +0200

    Add ATTACH utility function

---
+++ Clinfinity.c4d/System.c4g/Attach.c
@@ -0,0 +1,49 @@
+/*  Script: Attach.c
+    Helper functions related to attaching stuff! */
+
+#strict 2
+
+/*  Function: AttachTo
+	Attaches the calling object to the object _to_.
+
+	If the calling object isn't already in an action with procedure ATTACH the function will set the action "Attach".
+
+	When callerVertex or targetVertex are omitted, this function will add new vertices so that the object won't move.
+	Vertex indices are starting at 1.
+	
+	Parameters:
+	to - Object to which the caller should be attached.
+	callerVertex - The caller object's vertex which will be attached.
+	targetVertex - The vertex to attach to. */
+global func AttachTo(object to, int callerVertex, int targetVertex) {
+	if(GetProcedure() != "ATTACH")
+		SetAction("Attach");
+	SetActionTargets(to);
+
+	// correct z-order
+	SetObjectOrder(to);
+	
+	// optionally add vertices
+	if(callerVertex == 0) {
+		if(targetVertex == 0)
+			// use first vertex
+			targetVertex = 1;
+		var tx = GetVertex(targetVertex - 1, false), ty = GetVertex(targetVertex - 1, true);
+		AddVertex(AbsX(tx + to->GetX()) - GetOffsetX(), AbsY(ty + to->GetY()) - GetOffsetY());
+		callerVertex = GetVertexNum();
+	}
+	else if(targetVertex == 0) {
+		var cx = GetVertex(callerVertex - 1, false), cy = GetVertex(callerVertex - 1, true);
+		to->AddVertex(to->AbsX(tx + GetX()) - to->GetOffsetX(), to->AbsY(ty + GetY()) - to->GetOffsetY());
+		targetVertex = to->GetVertexNum();
+	}
+
+	callerVertex--;
+	targetVertex--;
+	
+	SetActionData(256 * callerVertex + targetVertex);
+}
+
+global func GetOffsetX() { return GetDefCoreVal("Offset", "DefCore", 0, 0); }
+global func GetOffsetY() { return GetDefCoreVal("Offset", "DefCore", 0, 1); }
+
