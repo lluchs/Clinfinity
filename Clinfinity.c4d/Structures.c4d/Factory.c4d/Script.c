1486c7733f55062e41144f0f214e42f3334614b7
Luchs <Luchs@euirc.eu>
2012-05-10 19:16:29 +0200

    Fix Factory's script

--- Clinfinity.c4d/Structures.c4d/Factory.c4d/Script.c
+++ Clinfinity.c4d/Structures.c4d/Factory.c4d/Script.c
@@ -1,18 +1,17 @@
-/*-- Neues Script --*/
+/*-- Factory --*/
 
-#strict
+#strict 2
 
-  
-public func ControlDigDouble(){
-  SetAction("ProduceTest");
-  };
-  
-public func ControlUp(){
-  SetAction("None");
-  Sound("finish*");
-  };
-  
-protected func Smoketest(){
-  CreateParticle("Smoke",-18,-18,0,0,150,RGBa(0,0,0,0));
-  CreateParticle("Smoke",-40,-18,0,0,150,RGBa(0,0,0,0));
-  };
+public func ControlDigDouble() {
+	SetAction("ProduceTest");
+}
+
+public func ControlUp() {
+	SetAction("None");
+	Sound("finish*");
+}
+
+protected func Smoketest() {
+	CreateParticle("Smoke",-18,-18,0,0,150,RGBa(0,0,0,0));
+	CreateParticle("Smoke",-40,-18,0,0,150,RGBa(0,0,0,0));
+}
