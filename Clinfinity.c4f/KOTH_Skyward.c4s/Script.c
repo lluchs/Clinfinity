1520d328c902ba22cf2a79f3602e1a4faf604ad0
Luchs <Luchs@euirc.eu>
2012-05-14 17:45:21 +0200

    KOTH_Skyward: Add KOTH goal

--- Clinfinity.c4f/KOTH_Skyward.c4s/Script.c
+++ Clinfinity.c4f/KOTH_Skyward.c4s/Script.c
@@ -3,6 +3,10 @@
 #strict 2
 
 func Initialize() {
+	// create goal
+	var cp = CreateObject(SHIP, LandscapeWidth() / 2, LandscapeHeight() / 2, NO_OWNER);
+	var koth = CreateObject(KOTH, 0, 0, NO_OWNER);
+	koth->SetCP(cp);
 }
 
 func InitializePlayer(int iPlr) {
