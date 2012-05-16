b31fc4843fc923dc51e504182fc98e27c91b8742
B_E <benedict.etzel@gmail.com>
2012-05-16 18:08:07 +0200

    Implemented simple production

--- Clinfinity.c4d/Structures.c4d/Factory.c4d/Script.c
+++ Clinfinity.c4d/Structures.c4d/Factory.c4d/Script.c
@@ -2,16 +2,43 @@
 
 #strict 2
 
-public func ControlDigDouble() {
-	SetAction("ProduceTest");
+local requestedItem;
+local remainingTime;
+
+public func ControlDigDouble(object caller) {
+	ShowProductionMenu(caller);
 }
 
-public func ControlUp() {
-	SetAction("None");
+public func ShowProductionMenu(object caller) {
+	CreateMenu(CXCN,caller,this,1,"");
+	var knowledge = ROCK;
+	AddMenuItem("", "StartProduction", knowledge, caller, 0, caller);
+}
+
+public func StartProduction(id item) {
+	requestedItem = item;
+	remainingTime = 10;
+	SetAction("Produce");
+}
+
+protected func Produce() {
+	if(!remainingTime) return CompleteProduction();
+	remainingTime--;
+}
+
+public func CompleteProduction() {
+	SetAction("Idle");
 	Sound("finish*");
+	remainingTime = 0;
+	var producedItem = CreateObject(requestedItem, 0, 0, GetOwner());
 }
 
-protected func Smoketest() {
+public func IsProducing() {
+	return remainingTime > 0;
+}
+
+private func Smoking() {
+	if(!IsProducing()) return;
 	CreateParticle("Smoke",-18,-18,0,0,150,RGBa(0,0,0,0));
 	CreateParticle("Smoke",-40,-18,0,0,150,RGBa(0,0,0,0));
 }
