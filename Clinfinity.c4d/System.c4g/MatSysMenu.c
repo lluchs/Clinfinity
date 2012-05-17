679f5d8fad9910c6ae256c8f21ee67e18074c19f
B_E <benedict.etzel@gmail.com>
2012-05-17 12:25:26 +0200

    Refactored AddProductionMenuItem to AddMaterialMenuItem

--- Clinfinity.c4d/System.c4g/MatSysMenu.c
+++ Clinfinity.c4d/System.c4g/MatSysMenu.c
@@ -1,44 +1,48 @@
-/*-- Library for centralised production menus --*/
+/*-- Functionality for material system menus --*/
 
 #strict 2
 
-global func CreateProductionMenu(id symbol, object menuObject, object commandObject, int extra, string caption, int extraData, int style, bool permanent, id menuId) {
-	if(!menuObject) return;
-	if(GetMenu(menuObject) == symbol) return;
-	AddEffect("ProductionMenuCheck", menuObject, 300, 1, commandObject, symbol, symbol);
-	return CreateMenu(symbol, menuObject, commandObject, extra, caption, extraData, style, permanent, menuId);
-}
-global func AddProductionMenuItem(string caption, string command, id item, object menuObject, int iCount, int parameter, string infoCaption, int extra, par1, par2) {
-	if(command) EffectCall(menuObject, GetEffect("ProductionMenuCheck", menuObject), "AddItem", item);
+/*  Function: AddMaterialMenuItem
+    Adds a menu item to a menu as documented, but also shows required materials
+    in the players material system.  */
+global func AddMaterialMenuItem(string caption, string command, id item, object menuObject, int iCount, int parameter, string infoCaption, int extra, par1, par2) {
+	var symbol = GetMenu(menuObject);
+	if(!symbol) return;
+	if(command) {
+		var effect = GetEffect("MaterialMenuCheck", menuObject);
+		if(!effect) effect = AddEffect("MaterialMenuCheck", menuObject, 300, 1, menuObject, symbol, symbol);
+		EffectCall(menuObject, effect, "AddItem", item);
+	}
 	AddMenuItem(caption, command, item, menuObject, iCount, parameter, infoCaption, extra, par1, par2);
 }
 
-global func FxProductionMenuCheckStart(object target, int effectNum, int temp, id menuId) {
-	EffectVar(0, target, effectNum) = menuId;
-	EffectVar(1, target, effectNum) = [];
+global func FxMaterialMenuCheckStart(object target, int effectNum, int temp, id menuId) {
+	EffectVar(0, target, effectNum) = menuId; //menuId
+	EffectVar(1, target, effectNum) = []; //menuItems
 }
 
-global func FxProductionMenuCheckAddItem(object target, int effectNum, id menuId) {
+global func FxMaterialMenuCheckAddItem(object target, int effectNum, id menuId) {
 	var menuItems = EffectVar(1, target, effectNum);
 	menuItems[GetLength(menuItems)] = menuId;
 	EffectVar(1, target, effectNum) = menuItems;
 }
 
-global func FxProductionMenuCheckTimer(object target, int effectNum) {
+global func FxMaterialMenuCheckTimer(object target, int effectNum) {
 	var menuId = EffectVar(0, target, effectNum);
 	var matSys = GetMatSys(GetOwner(target));
 	if(GetMenu(target) != menuId || !matSys)
 		return -1;
+		
 	var menuItems = EffectVar(1, target, effectNum);
 	if(!GetLength(menuItems))
 		return;
 	var type = menuItems[GetMenuSelection(target)];
 	if(!type)
-		return;
+		return GetMatSys(GetOwner(target))->ClearMaterialCheck();
 	matSys->MaterialCheck(type);
 }
 
-global func FxProductionMenuCheckStop(object target, int effectNum) {
+global func FxMaterialMenuCheckStop(object target, int effectNum) {
 	var menuId = EffectVar(0, target, effectNum);
 	if(GetMenu(target) == menuId) CloseMenu(target);
 	GetMatSys(GetOwner(target))->ClearMaterialCheck();
