2b5f3d25f66847a3f0c0afdf1635525cc0286db4
Nachtfalter <f.graier@gmx.de>
2012-05-31 01:47:15 +0200

    Add experimental gamma

--- Clinfinity.c4f/KOTH_Skyward.c4s/Script.c
+++ Clinfinity.c4f/KOTH_Skyward.c4s/Script.c
@@ -7,6 +7,9 @@ func Initialize() {
 	var cp = CreateObject(SHIP, LandscapeWidth() / 2, LandscapeHeight() / 2, NO_OWNER);
 	var koth = CreateObject(KOTH, 0, 0, NO_OWNER);
 	koth->SetCP(cp);
+	
+	// steampunky flair
+	SetGamma(RGB(15,15,20),RGB(118,118,118),RGB(210,215,255));
 }
 
 func InitializePlayer(int plr) {
@@ -17,7 +20,7 @@ func InitializePlayer(int plr) {
 	msys->DoFill(10, METL);
 	msys->DoFill(10, ROCK);
 	
-	// Place Fog
+	// place fog
 	for(var i;i<160;++i) 
     CreateParticle("Cloud",Random(LandscapeWidth()),Random(LandscapeHeight()*2/3),RandomX(3,9),0,RandomX(1000,1500));
 	
