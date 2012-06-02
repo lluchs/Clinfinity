4802b39fcfc555e0b941d94e392035b91d1fb5c1
Luchs <Luchs@euirc.eu>
2012-06-02 20:53:20 +0200

    KOTH_Skyward: Spawn with platform, tank and catapult

--- Clinfinity.c4f/KOTH_Skyward.c4s/Script.c
+++ Clinfinity.c4f/KOTH_Skyward.c4s/Script.c
@@ -2,20 +2,31 @@
 
 #strict 2
 
+static joinedTeams;
+
 func Initialize() {
 	// create goal
 	var cp = CreateObject(SHIP, LandscapeWidth() / 2, LandscapeHeight() / 2, NO_OWNER);
 	var koth = CreateObject(KOTH, 0, 0, NO_OWNER);
 	koth->SetCP(cp);
+
+	joinedTeams = [];
 	
 	// steampunky flair
-	SetGamma(RGB(15,15,20),RGB(118,118,118),RGB(210,215,255));
+	SetGamma(RGB(15, 15, 20), RGB(118, 118, 118), RGB(210, 215, 255));
 	
-  // place fog
-	for(var i;i<160;++i) 
-    CreateParticle("Cloud",Random(LandscapeWidth()),Random(LandscapeHeight()*2/3),RandomX(3,9),0,RandomX(1000,1500),RGBa(116,131,145,0));
+	// place fog
+	for(var i; i<160; ++i) 
+		CreateParticle("Cloud", Random(LandscapeWidth()), Random(LandscapeHeight()*2/3), RandomX(3, 9), 0, RandomX(1000, 1500), RGBa(116, 131, 145, 0));
 	
 	ScriptGo(1);
+}
+
+func GetStartPosition(int team) {
+	if(team == 1)
+		return [200, 400];
+	if(team == 2)
+		return [1710, 430];
 }
 
 func InitializePlayer(int plr) {
@@ -26,14 +37,30 @@ func InitializePlayer(int plr) {
 	msys->DoFill(10, METL);
 	msys->DoFill(10, ROCK);
 
+	var team = GetPlayerTeam(plr);
+	var pos = GetStartPosition(team);
+	if(GetLength(GetPlayersByTeam(team)) == 1) {
+		CreateStartMaterial(pos[0], pos[1], plr);
+	}
+
+	var tank = FindObject2(Find_ID(STMT), Find_Allied(plr));
+	tank->DoFill(200);
+	var i = 0, clonk;
+	while(clonk = GetCrew(plr, i++))
+		clonk->Enter(tank);
+
 }
 
-func Script0()
-{
-  CreateParticle("Cloud",0,Random(LandscapeHeight()*2/3),RandomX(3,9),0,RandomX(1000,1500), RGBa(116,131,145,0)); 
+func CreateStartMaterial(int x, int y, int plr) {
+	PLTF->CreatePlatform(x, y, plr);
+	CreateConstruction(STMT, x + 10, y - 5, plr, 100);
+	CreateObject(CATA, x + Random(20), y - 5, plr);
 }
 
-func Script4()
-{
-  return(goto(0));
+func Script0() {
+	CreateParticle("Cloud", 0, Random(LandscapeHeight()*2/3), RandomX(3, 9), 0, RandomX(1000, 1500),  RGBa(116, 131, 145, 0)); 
+}
+
+func Script4() {
+	return goto(0);
 }
