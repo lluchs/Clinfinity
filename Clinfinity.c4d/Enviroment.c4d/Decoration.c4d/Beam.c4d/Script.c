dfc444a9894db6922d0ca4a634f073a31267b5db
Nachtfalter <f.graier@gmx.de>
2012-06-04 16:07:50 +0200

    Plank squeaks - needs improvement

--- Clinfinity.c4d/Enviroment.c4d/Decoration.c4d/Beam.c4d/Script.c
+++ Clinfinity.c4d/Enviroment.c4d/Decoration.c4d/Beam.c4d/Script.c
@@ -8,3 +8,10 @@ protected func Left() {
 protected func Right() {
 	SetSolidMask(41, 22, 41, 22, 0, 0);
 }
+
+protected func CheckClonk() {
+  // any clonk on me?
+  if(FindObject2(Find_OCF(OCF_CrewMember), Find_InRect(-20,-30,40,25))){
+  Sound("squeak*");
+  }
+}
