eb7d8637f539e5d086ca778ce84494011849c0c5
Luchs <Luchs@euirc.eu>
2012-06-05 23:28:08 +0200

    Clean up and fix BirdSong

--- Clinfinity.c4d/Enviroment.c4d/BirdSong.c4d/Script.c
+++ Clinfinity.c4d/Enviroment.c4d/BirdSong.c4d/Script.c
@@ -2,24 +2,23 @@
 
 #strict 2
 
-protected func Initialize()
-{
-  SetAction("Sing");
-  SetPhase(Random(20));
-  return(1);
+protected func Initialize() {
+    SetAction("Sing");
+	// randomize start
+    SetPhase(Random(20));
+    return 1;
 }
 
-private func Singing(){
-  // is night? Sleep!
-  if (IsNight())
-    return(0);
-    
-  // check if there are some trees left and is day
-    if(Random(8)){
-      if(ObjectCount2(Find_Func("IsTree")) >= 10)
-      Sound("BirdSong*",1);
-      }
-    else{
-      Sound("Crow*",1);
-      }
-}  
+private func Singing() {
+    // Is night? Sleep!
+    if(IsNight())
+        return;
+
+    if(!Random(8)) {
+		// check if there are some trees left
+        if(ObjectCount2(Find_Category(C4D_StaticBack), Find_Func("IsTree")) >= 10)
+            Sound("BirdSong*", 1);
+		else
+			Sound("Crow*", 1);
+    }
+}
