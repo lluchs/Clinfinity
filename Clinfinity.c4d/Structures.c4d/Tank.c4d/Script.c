9b3a215b0fb7e68a8f5d40bbda77dc7cf9f16891
Luchs <Luchs@euirc.eu>
2012-06-07 13:33:44 +0200

    Add warning when steam trend is negative

--- Clinfinity.c4d/Structures.c4d/Tank.c4d/Script.c
+++ Clinfinity.c4d/Structures.c4d/Tank.c4d/Script.c
@@ -5,6 +5,8 @@
 #include L_SS
 #include STBO
 
+static steamTrendWarnEffects;
+
 // number of animation phases
 static const STMT_Phases = 10;
 // amount needed to respawn one clonk
@@ -12,8 +14,22 @@ static const STMT_RespawnAmount = 100;
 
 public func MaxFill() { return 1500; }
 
+// all previous changes in fill level
+local changes;
+
 protected func Initialize() {
+	changes = [];
 	SetAction("Tank");
+
+	// steam trend warning
+	if(!steamTrendWarnEffects)
+		steamTrendWarnEffects = [];
+	var team = GetPlayerTeam(GetOwner());
+	if(!steamTrendWarnEffects[team]) {
+		steamTrendWarnEffects[team] = AddEffect("SteamTrendWarning", 0, 1, 100);
+		EffectVar(0, 0, steamTrendWarnEffects[team]) = team;
+	}
+
 	// create dummy effect if this is the first Akku
 	// this ensures that when the last Akku gets destroyed, the display is reset to 0
 	if(!GetEffect("MatSysSTEM"))
@@ -55,13 +71,40 @@ protected func Steam() {
 	}
 }
 
-private func OnFillChange() {
+private func OnFillChange(key, int change) {
 	UpdateDisplay();
+	PushBack([FrameCounter(), change], changes);
 }
 
 private func UpdateDisplay() {
 	SetPhase(ChangeRange(GetFill(), 0, MaxFill() - 1, STMT_Phases - 1, 0));
 	return 1;
+}
+
+public func GetChanges(int frames) {
+	var min = FrameCounter() - frames, result = [];
+	for(var i = GetLength(changes) - 1; i && changes[i][0] >= frames; i--)
+		PushBack(changes[i][1], result);
+	return result;
+}
+
+public func GlobalSteamChanges(int frames, bool average) {
+	var result = 0, num = 0;
+	for(var tank in FindObjects(Find_ID(GetID()), Find_Allied(GetOwner()))) {
+		for(var change in tank->GetChanges(frames)) {
+			result += change;
+			num++;
+		}
+	}
+	if(average)
+		return result / num;
+	else
+		return result;
+}
+
+public func ShowChanges() {
+	Message("@Trend: %d", 0, GlobalSteamChanges(750));
+	ScheduleCall(this, "ShowChanges", 25);
 }
 
 // Steuerung
@@ -91,5 +134,20 @@ public func FxMatSysSTEMChange(object target, int effectNum, int plr, int change
 	if(!this || GetOwner() != plr)
 		return 0;
 	return DoFill(change);
+}
+
+/* Steam trend warning */
+global func FxSteamTrendWarningTimer(object target, int effectNum, int effectTime) {
+	var team = EffectVar(0, target, effectNum), players = GetPlayersByTeam(team);
+	var tank = FindObject2(Find_ID(STMT), Find_Allied(players[0]));
+	var warning = EffectVar(1, target, effectNum), next;
+	if(tank && tank->GlobalSteamChanges(750, true) < 0)
+		next = true;
+	else
+		next = false;
+	if(!warning && next || warning && !next)
+		for(var plr in players)
+			Sound("Warning_blowup", true, 0, 75, plr + 1, next*2 - 1);
+	EffectVar(1, target, effectNum) = next;
 }
 
