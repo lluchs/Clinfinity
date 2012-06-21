c0e87f48fed77f4d59a67b96f25ad9056ab53fde
Nachtschatten <ChyH@gmx.net>
2012-06-21 21:30:02 +0200

    Started writing the yo-yo script.

--- Clinfinity.c4d/Items.c4d/Weapons.c4d/Yo-Yo.c4d/Script.c
+++ Clinfinity.c4d/Items.c4d/Weapons.c4d/Yo-Yo.c4d/Script.c
@@ -1,8 +1,21 @@
-/*-- Neues Objekt --*/
+#strict 2
 
-#strict
-
-func Initialize() {
-   
-  return(1);
+protected func Departure(object container) {
+	// If the yo-yo was thrown by a Clonk, act as weapon.
+	if((container->GetOCF() & OCF_CrewMember) != 0) {
+		// Set speed to fly lower, because it's supposed to hit enemies.
+		SetXDir(GetXDir() * 3);
+		SetYDir(-5);
+		// Attach yo-yo line to Clonk.
+	}
 }
+
+protected func Hit(/* TODO: Parameters? */) {
+	// TODO
+	// If working as a weapon: Remove vertex, return to sender
+	// TODO: While returning, perhaps change the category to vehicle so we don't hit the thrower
+}
+
+// TODO: Reject entrance to anyone but the throwing clonk.
+
+// TODO: Get slower on x axis over time, and pick up speed again when returning to thrower?
