044fc94f9ef4b83553504f8b7451648670eee5ac
Nachtschatten <ChyH@gmx.net>
2012-06-27 02:38:24 +0200

    Yo-yo: Recoil from Clonks.

--- Clinfinity.c4d/Items.c4d/Weapons.c4d/Yo-Yo.c4d/Script.c
+++ Clinfinity.c4d/Items.c4d/Weapons.c4d/Yo-Yo.c4d/Script.c
@@ -36,6 +36,19 @@ protected func Hit(int xSpeed, int ySpeed) {
 	}
 }
 
+protected func QueryStrikeBlow(object target) {
+	// Recoil from Clonks when used as weapon.
+	if(currentState == YOYO_StateThrown) {
+		Sound("Yo-yo hit");
+		// TODO: Inflict damage on target and perhaps fling it a bit
+		YoyoReturn();
+		return true;
+	} else if(currentState == YOYO_StateReturning) {
+		Sound("Yo-yo hit");
+		return true;
+	}
+}
+
 protected func RejectEntrance(object into) {
 	return (currentState != YOYO_StateInactive && into != thrower);
 }
