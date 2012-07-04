a716be1767f3f723f2773da33cfd8dc258fd0bb6
Nachtfalter <f.graier@gmx.de>
2012-07-05 00:31:16 +0200

    * Update

--- Clinfinity.c4f/KOTH_Observatory.c4s/Observatory.c4d/Script.c
+++ Clinfinity.c4f/KOTH_Observatory.c4s/Observatory.c4d/Script.c
@@ -1,8 +1,28 @@
-/*-- Neues Objekt --*/
+/*-- Observatory --*/
 
-#strict
+#strict 2
 
-func Initialize() {
-   
-  return(1);
+// this is a control point
+#include L_CP
+
+local flag;
+
+public func Initialize() {
+	SetAction("MakeSound");
+
+	flag = CreateObject(FLAG, 0, 0, NO_OWNER);
+	flag->SetAction("FlyBase", this);
 }
+
+private func Captured() {
+	flag->SetOwner(GetOwner());
+	return inherited(...);
+}
+
+public func CaptureTime() { return 400; }
+
+public func CaptureZone() {
+	// on top of the platform
+	return Find_InRect(-65, -150, 121, 30);
+}
+
--- Clinfinity.c4f/KOTH_Observatory.c4s/Script.c
+++ Clinfinity.c4f/KOTH_Observatory.c4s/Script.c
@@ -3,27 +3,60 @@
 #strict 2
 
 func Initialize() {
-	/* create goal
-	var cp = CreateObject(SHIP, LandscapeWidth() / 2, LandscapeHeight() / 2, NO_OWNER);
+	var cp = CreateObject(OBSV,1210,1170,-1);
+	cp -> SetClrModulation(RGBa(235,230,255,10));
 	var koth = CreateObject(KOTH, 0, 0, NO_OWNER);
 	koth->SetCP(cp);
 	
-	//var mast1 = CreateObject(TEL1,168,417,-1);
-  //var mast2 = CreateObject(TEL1,259,419,-1);
-  //CreateObject(WIRE)->Connect(mast1,mast2);
-  }
+  //Observatory Parts
+	var tw1 = CreateObject(TW_1,1087,825);
+	tw1 -> SetClrModulation(RGBa(230,220,255,80));
+	var tw2 = CreateObject(TW_2,1019,1050);
+	tw2 -> SetClrModulation(RGBa(230,220,255,5));
+
+	//Mountains
+	var MtL = CreateObject(MONT,900,1350);
+	var MtR = CreateObject(MONT,1400,1350);
 	
-	// steampunky flair
-	SetGamma(RGB(15,15,20),RGB(118,118,118),RGB(210,215,255));
-	
-  // place fog
-	for(var i;i<160;++i) 
-    CreateParticle("Cloud",Random(LandscapeWidth()),Random(LandscapeHeight()*2/3),RandomX(3,9),0,RandomX(1000,1500),RGBa(116,131,145,0));
-	
-	ScriptGo(1);
-	*/
-	
-  CreateObject(OBSV,1210,1170);
+	//Moon
+	var moon = CreateObject(MOON,1337,490);
+	moon -> SetClrModulation(RGBa(255,255,255,80));
+  
+  //Trees
+  CreateObject(TRE4,1076,905,-1);
+  CreateObject(TRE4,1067,926,-1);
+  CreateObject(TRE4,1184,917,-1);
+  CreateObject(TRE4,1182,904,-1);
+  CreateObject(TRE4,1190,918,-1);
+  CreateObject(TRE4,1175,922,-1);
+  CreateObject(TRE4,1231,926,-1);
+  CreateObject(TRE4,1206,925,-1);
+  CreateObject(TRE4,1211,905,-1);
+  CreateObject(TRE4,1251,921,-1);
+  CreateObject(TRE4,1341,922,-1);
+  CreateObject(TRE4,1317,920,-1);;
+  CreateObject(TRE4,1377,925,-1);
+  CreateObject(TRE4,1083,931,-1);
+  CreateObject(TRE4,1163,964,-1);
+  CreateObject(TRE4,1190,961,-1);
+  CreateObject(TRE4,1180,963,-1);
+  CreateObject(TRE4,1227,946,-1);
+  CreateObject(TRE4,1293,961,-1);;
+  CreateObject(TRE4,1264,936,-1);
+  CreateObject(TRE4,1291,941,-1);
+  CreateObject(TRE4,1340,968,-1);
+  CreateObject(TRE4,1345,945,-1);
+  CreateObject(TRE4,1313,972,-1);
+  CreateObject(TRE4,1372,961,-1);
+  CreateObject(TRE4,1392,1051,-1);
+  CreateObject(TRE4,1404,1076,-1);
+}
+
+func GetStartPosition(int team) {
+	if(team == 1)
+		return [200, 400];
+	if(team == 2)
+		return [2240, 400];
 }
 
 func InitializePlayer(int plr) {
@@ -31,17 +64,25 @@ func InitializePlayer(int plr) {
 	// fill with material
 	var msys = GetMatSys(plr);
 	msys->DoFill(5, WOOD);
-	msys->DoFill(10, METL);
+	msys->DoFill(15, METL);
 	msys->DoFill(10, ROCK);
 
+	var team = GetPlayerTeam(plr);
+	var pos = GetStartPosition(team);
+	if(GetLength(GetPlayersByTeam(team)) == 1) {
+		CreateStartMaterial(pos[0], pos[1], plr);
+	}
+
+	var tank = FindObject2(Find_ID(STMT), Find_Allied(plr));
+	tank->DoFill(300);
+	var i = 0, clonk;
+	while(clonk = GetCrew(plr, i++))
+		clonk->Enter(tank);
+
 }
 
-func Script0()
-{
-  CreateParticle("Cloud",0,Random(LandscapeHeight()*2/3),RandomX(3,9),0,RandomX(1000,1500), RGBa(116,131,145,0)); 
-}
-
-func Script4()
-{
-  return(goto(0));
+func CreateStartMaterial(int x, int y, int plr) {
+	PLTF->CreatePlatform(x, y, plr);
+	CreateConstruction(STMT, x + 10, y - 5, plr, 100);
+	CreateObject(CATA, x + Random(20), y - 5, plr);
 }
