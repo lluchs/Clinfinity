a9eadf1be5353a2bf759bc32d8a4586b9291ef3f
Nachtfalter <f.graier@gmx.de>
2012-07-05 17:13:17 +0200

    ! Now all 15 birdsongs will be randomly played

--- Clinfinity.c4d/Enviroment.c4d/BirdSong.c4d/Script.c
+++ Clinfinity.c4d/Enviroment.c4d/BirdSong.c4d/Script.c
@@ -16,8 +16,12 @@ private func Singing() {
 
     if(!Random(8)) {
 		// check if there are some trees left
-        if(ObjectCount2(Find_Category(C4D_StaticBack), Find_Func("IsTree")) >= 10)
-            Sound("BirdSong*", 1);
+        if(ObjectCount2(Find_Category(C4D_StaticBack), Find_Func("IsTree")) >= 10){
+            //Sound("BirdSong*", 1);
+            var sound = Random(15)+1;
+            Sound(Format("BirdSong%d", sound));
+            //Log("%d", sound);
+            }
 		else
 			Sound("Crow*", 1);
     }
