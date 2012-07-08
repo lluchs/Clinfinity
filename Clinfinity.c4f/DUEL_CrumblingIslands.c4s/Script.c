672a8748399f5e18990467a3c5e41cb3a5f3e484
Nachtfalter <f.graier@gmx.de>
2012-07-08 04:14:12 +0200

    Updates

--- Clinfinity.c4f/DUEL_CrumblingIslands.c4s/Script.c
+++ Clinfinity.c4f/DUEL_CrumblingIslands.c4s/Script.c
@@ -3,9 +3,16 @@
 #strict
 
 func Initialize() {
+  //Ressource Buildings
+  CreateConstruction(RFLN,810,550,-1,100,1,0); //Flintfactory
+
   //Crumbling Islands
   var CIsleL = CreateObject(ISLE,760,810);
   CIsleL -> SetClrModulation(RGBa(200,215,255,50));
   
   SetSkyParallax(1,20,0,1,0);
+}
+
+func InitializePlayer(int plr) {
+	CreateMatSys(plr);
 }
