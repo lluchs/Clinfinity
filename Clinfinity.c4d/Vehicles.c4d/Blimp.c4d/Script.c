00a247b427cbef76c6a75c46696535d77d327f42
Nachtfalter <f.graier@gmx.de>
2012-08-01 13:57:03 +0200

    Add a blimp (wip)

---
+++ Clinfinity.c4d/Vehicles.c4d/Blimp.c4d/Script.c
@@ -0,0 +1,21 @@
+/*-- Blimp --*/
+
+#strict 2
+//local t;
+
+protected func Initialize(){
+  SetAction("Spin");
+}
+
+// pendle around, no solidmask though (yet)
+public func Pendle() {
+  t++;
+  var r = Cos(t, 200);
+  var fsin=Sin(r, 1000, 100);
+  var fcos=Cos(r, 1000, 100);
+  SetObjDrawTransform (
+    +fcos, +fsin, (1000-fcos) - fsin,
+    -fsin, +fcos, (1000-fcos) + fsin
+  );
+}
+
