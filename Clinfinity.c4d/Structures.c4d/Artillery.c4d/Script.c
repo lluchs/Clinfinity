f0600aee6554748bfc98e12119e509c81430fedf
Luchs <Luchs@euirc.eu>
2012-08-03 01:25:11 +0200

    Artillery: Remove obsolete script parts
     - Players can't disconnect cannons
     - There is no door

--- Clinfinity.c4d/Structures.c4d/Artillery.c4d/Script.c
+++ Clinfinity.c4d/Structures.c4d/Artillery.c4d/Script.c
@@ -11,18 +11,7 @@ local cannon;
 #include L_SS
 
 public func MaxDamage() {
-	return(50);
-}
-
-/* Fahrzeug suchen und Inhalt nehmen */
-private func CloseEntrance() {
-	var cont = FindObject2(Find_Category(C4D_Vehicle), Find_Container(this()),
-			Find_Not(Find_ID(CTW3), Find_ID(CTW5), Find_ID(CTW7), Find_ID(ACT1), Find_ID(ACT3), Find_ID(LICN)));
-	if(cont) {
-		GrabContents(cont);
-		Exit(cont, 0, 21);
-	}
-	return(_inherited() );
+	return 50;
 }
 
 //Hilft der Kanone, loescht die Zielhilfe
@@ -35,7 +24,6 @@ func Grabbed(object by, bool grab) {
 
 /* Kanonensteuerung */
 
-
 protected func Progress() {
 	SetAction("Attach");
 	/*
@@ -47,7 +35,7 @@ protected func Progress() {
 
 protected func ControlLeft(object obj) {
 	[$TxtRotatecannontothelef$ | Image = CT01: 0]
-	if(!cannon) return(0);
+	if(!cannon) return 0;
 	var plr = obj->GetOwner();
 	// wrong team?
 	if(Hostile(plr, GetOwner())) {
@@ -61,27 +49,27 @@ protected func ControlLeft(object obj) {
 }
 
 protected func ControlLeftReleased(object obj) {
-	if(!cannon) return(0);
+	if(!cannon) return 0;
 	Sound("CannonStop");
 	return(cannon->ComStop(obj));
 }
 
 protected func ControlRight(object obj) {
 	[$TxtRotatecannontotherig$ | Image = CT01: 2]
-	if(!cannon) return(0);
+	if(!cannon) return 0;
 	Sound("Click");
 	return(cannon->ComRight(obj) );
 }
 
 protected func ControlRightReleased(object obj) {
-	if(!cannon) return(0);
+	if(!cannon) return 0;
 	Sound("CannonStop");
 	return(cannon->ComStop(obj));
 }
 
 protected func ControlUp(object obj) {
 	[$TxtStopcannonrotation$ | Method = Classic]
-	if(!cannon) return(0);
+	if(!cannon) return 0;
 
 	if(!GetPlrCoreJumpAndRunControl(obj->GetController())) {
 		Sound("Click");
@@ -90,15 +78,9 @@ protected func ControlUp(object obj) {
 	}
 }
 
-protected func ControlUpDouble(object obj) {
-	[$TxtDisconnectcannon$ | Image = CT01: 3]
-	if(!cannon) return(0);
-	return(ReleaseCannon());
-}
-
 protected func ControlDig(object obj) {
 	[$TxtFire$ | Image = CT01: 1]
-	if (!cannon) return(0);
+	if (!cannon) return 0;
 	return (cannon->ComFire(obj));
 }
 
@@ -106,42 +88,10 @@ protected func ControlDown(object obj) {
 	[Method = Classic]
 	/* Kanone anhalten und den Clonk noch rauslassen */
 	ControlUp(obj);
-	return(0);
+	return 0;
 }
 
-/* Kontext */
-
-public func ContextReleaseCannon(caller) {
-	[$TxtDisconnectcannon$ | Image = CT01: 3 | Condition = HasCannon]
-	// Der Clonk soll erst in Geschützturm laufen
-	/*
-	   if (caller->Contained() != this())
-	   {
-	   SetCommand(caller, "None");
-	   AddCommand(caller, "Call", this(), 0,0,0,0, "ReleaseCannon");
-	   AddCommand(caller, "Enter", this());
-	   return(0);
-	   } */
-	// Der Clonk kann die Kanone direkt abschrauben
-	return(ReleaseCannon());
-}
-
-/* Kanone anschrauben */
-
-protected func Collection2(object obj) {
-	if(obj->~IsCannon() ) {
-		if(!cannon)
-			ConnectCannon(obj);
-	} else {
-		if(cannon)
-			cannon->~ComEnter(obj);
-	}
-}
-
-protected func Ejection(object obj) {
-	if(cannon)
-		cannon->~ComLeave(obj);
-}
+/* Cannon connection functionality */
 
 public func ConnectCannon(object newCannon) {
 	cannon = newCannon;
@@ -174,7 +124,7 @@ public func ConnectCannonDef(object cannon, id defChg, string action) {
 
 // Löst die Kanone vom Geschützturm
 public func ReleaseCannon() {
-	if(!cannon) return(0);
+	if(!cannon) return 0;
 	Sound("Connect");
 	cannon->ComStopDouble();
 	// Kategorie für die Kanone wiederherstellen
@@ -189,24 +139,24 @@ public func ReleaseCannon() {
 			break;
 		}
 	}
-	return(1);
+	return 1;
 }
 
 /* Wird von den Kanonen als Turm erkannt */
 
 public func IsCannonTower() {
-	return(1);
+	return 1;
 }
 
 public func HasCannon() {
-	//return (cannon != 0);
-	return (cannon);
+	return cannon;
 }
 
 /* Zerstörung */
 
 protected func Destruction() {
-	if(cannon) ReleaseCannon();
-	return(_inherited() );
+	if(cannon)
+		cannon->RemoveObject();
+	return _inherited(...);
 }
 
