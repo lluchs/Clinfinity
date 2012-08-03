8edd56533fc417efe1175596cfafb97f5f90291a
Nachtfalter <f.graier@gmx.de>
2012-08-03 21:11:42 +0200

    Initialstuff

---
+++ Clinfinity.c4f/Tutorial_Weapons.c4s/Script.c
@@ -0,0 +1,13 @@
+/*-- Weapon Tutorial --*/
+
+#strict 2
+
+// Positions for Relaunch
+static iPlrX, iPlrY, iCounter;
+
+func Initialize() {
+	// Decoration
+  CreateObject(WTFL, 350, 290); //Waterfall
+
+	ScriptGo(true);
+}
