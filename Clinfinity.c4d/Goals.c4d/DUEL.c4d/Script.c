4f6e076f36ae324e20d507c0f4ab29b49ee51bb8
Luchs <Luchs@euirc.eu>
2012-08-03 15:59:01 +0200

    Implement DUEL goal

--- Clinfinity.c4d/Goals.c4d/DUEL.c4d/Script.c
+++ Clinfinity.c4d/Goals.c4d/DUEL.c4d/Script.c
@@ -1,8 +1,13 @@
-/*-- Neues Objekt --*/
+/*-- DUEL GOAL --*/
 
-#strict
+#strict 2
 
-func Initialize() {
-   
-  return(1);
+#include GOAL
+
+protected func Completion() {
+	CreateObject(RVLR, 0, 0, NO_OWNER);
+}
+
+public func IsFulfilledforPlr(int plr) {
+	return !FindObject2(Find_ID(STMT), Find_Hostile(plr));
 }
