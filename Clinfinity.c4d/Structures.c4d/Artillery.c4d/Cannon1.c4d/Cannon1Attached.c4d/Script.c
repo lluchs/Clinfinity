0c0086f805dd54579a3f66e5a1860519f136e93f
Luchs <Luchs@euirc.eu>
2012-08-03 14:02:15 +0200

    Cannon1Attached: Convert to #strict 2

--- Clinfinity.c4d/Structures.c4d/Artillery.c4d/Cannon1.c4d/Cannon1Attached.c4d/Script.c
+++ Clinfinity.c4d/Structures.c4d/Artillery.c4d/Cannon1.c4d/Cannon1Attached.c4d/Script.c
@@ -1,20 +1,19 @@
 /*--- Kampfgeschütz (angeschraubt) ---*/
 
-#strict
+#strict 2
 
 // Steam a single shot consumes this amount of Steam
 static const CTW2_BasicSteamUsage = 5; //SteamUsage*ObjectMass
 
 /* Überladbar für andere Kanonen */
 
-protected func RotationSpeed() { return(5); }
-protected func CannonMobileID() { return(CTW3); }
-protected func CannonAmmo(object obj) { return(GetOCF(obj) & OCF_Collectible() ); }
-protected func CannonPowderNeeded(object obj) { return(1); }
-protected func CannonPower(object obj) { return(12); }
-protected func CannonSound(object obj) { return("Blast3"); }
-protected func CannonSmoke(object obj) { return(1); }
-protected func CannonShootMenuID() { return(GetID()); }
+protected func RotationSpeed() { return 5; }
+protected func CannonMobileID() { return CTW3; }
+protected func CannonAmmo(object obj) { return GetOCF(obj & OCF_Collectible ); }
+protected func CannonPower(object obj) { return 12; }
+protected func CannonSound(object obj) { return "Blast3"; }
+protected func CannonSmoke(object obj) { return 1; }
+protected func CannonShootMenuID() { return GetID(); }
 protected func CannonShootMenuName(object potentialProjectile) { return Format("$TxtShoots$",GetName(potentialProjectile)); }
 
 
@@ -44,21 +43,21 @@ public func ComLeft(object clonk) {
 	SetAction("Rotating", GetActionTarget());
 	SetRDir(-RotationSpeed() );
 	Trajectory(clonk);
-	return(1);
+	return 1;
 }
 
 public func ComRight(object clonk) {
 	SetAction("Rotating", GetActionTarget());
 	SetRDir(RotationSpeed() );
 	Trajectory(clonk);
-	return(1);
+	return 1;
 }
 
 public func ComStop(object clonk) {
 	SetAction("Attaching", GetActionTarget());
 	SetRDir(0);
 	Trajectory(clonk);
-	return(1);
+	return 1;
 }
 
 //Wird vom Grabber nicht uebertragen, nur vom Container, Achtung, Aufruf stattdessen im Cannontower
@@ -70,7 +69,7 @@ public func ComStopDouble(object clonk) {
 	var r = (GetR() + 270) % 360;
 	SetR(r);
 	ChangeDef(CannonMobileID() );
-	return(1);
+	return 1;
 }
 
 public func ComEnter() {
@@ -106,12 +105,12 @@ private func DoMenu(object clonk, int selection) {
 				AddMenuItem(CannonShootMenuName(obj), Format("Shoot(%i, Object(%d), Object(%d))", obj->GetID(), ObjectNumber(obj), ObjectNumber(clonk)), 0, clonk, 0, 0, 0, 4, obj);
 	SelectMenuItem(selection, clonk);
 	Trajectory(clonk);
-	return(1);
+	return 1;
 }
 
 func OnMenuSelection(int itemIndex, object menuObject) {
 	Trajectory(menuObject);
-	return(1);
+	return 1;
 }
 
 func Trajectory(object clonk, bool fClose) {
@@ -126,24 +125,24 @@ func Trajectory(object clonk, bool fClose) {
 		//Log("Remove");
 		RemoveTrajectory(this());
 	}
-	return(1);
+	return 1;
 }
 
 private func Shoot(id defFoo, object projectile, object shooter) {
-	if (!projectile) return(0);
-	if (projectile->Contained() != GetActionTarget()) return(0);
+	if (!projectile) return 0;
+	if (projectile->Contained() != GetActionTarget()) return 0;
 
 
 	//enough Steam?
 	var plr = GetOwner();
 	var msys = GetMatSys(plr);
-	var powder = CannonPowderNeeded(projectile);
+	var steam = CTW2_BasicSteamUsage * GetMass(projectile);
 
-	if(MatSysGetTeamFill(plr, STEM) < CTW2_BasicSteamUsage * GetMass(projectile)) {
+	if(MatSysGetTeamFill(plr, STEM) < steam) {
 		Sound("Error");
-		Message("$TxtNotenoughgunpowder1r$", GetActionTarget(), powder);
+		Message("$TxtNotenoughgunpowder1r$", GetActionTarget(), steam);
 	} else {
-		MatSysDoTeamFill(-CTW2_BasicSteamUsage * GetMass(projectile), plr, STEM);
+		MatSysDoTeamFill(-steam, plr, STEM);
 
 		if(CannonSmoke(projectile) ) {
 			Smoke(Sin(GetR(), 13), -Cos(GetR(), 13), 20);
@@ -158,7 +157,7 @@ private func Shoot(id defFoo, object projectile, object shooter) {
 
 		//AddTrajectory(this(),Sin(GetR(), 13), -Cos(GetR(), 13),Sin(GetR(), CannonPower(projectile)), -Cos(GetR(), CannonPower(projectile)));
 		Exit(projectile, Sin(GetR(), 13), -Cos(GetR(), 13), GetR(), Sin(GetR(), CannonPower(projectile)), -Cos(GetR(), CannonPower(projectile)), 20);
-		if(GetOCF(projectile) & OCF_CrewMember() ) ObjectSetAction(projectile, "Tumble");
+		if(GetOCF(projectile) & OCF_CrewMember ) ObjectSetAction(projectile, "Tumble");
 		Sound(CannonSound(projectile) );
 		SetPlrView(shooter->GetOwner(), projectile);
 	}
