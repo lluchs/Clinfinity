a0b134468d15a62b4cc67c003a8c95f915cdbb4a
Luchs <Luchs@euirc.eu>
2012-08-03 19:51:15 +0200

    Implement the steam generator ressource

--- Clinfinity.c4d/Structures.c4d/Steamgen.c4d/Script.c
+++ Clinfinity.c4d/Structures.c4d/Steamgen.c4d/Script.c
@@ -4,3 +4,23 @@
 
 #include L_CP
 #include B_70
+
+static const RSMG_ProductionTime = 200;
+static const RSMG_ProductionRate = 50;
+
+public func CaptureTime() { return 100; }
+
+private func Captured() {
+	StartProduction();
+	return inherited(...);
+}
+
+private func StartProduction() {
+	ClearScheduleCall(this, "ProduceSteam");
+	ScheduleCall(this, "ProduceSteam", RSMG_ProductionTime);
+}
+
+protected func ProduceSteam() {
+	MatSysDoTeamFill(RSMG_ProductionRate + RandomX(-10, 10), GetOwner(), STEM);
+	StartProduction();
+}
