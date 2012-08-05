4496758160c52b07a56a4f68e263e84675f31d63
Nachtfalter <f.graier@gmx.de>
2012-08-05 14:24:59 +0200

    Tutorial_Weapons: WIP

--- Clinfinity.c4f/Tutorial_Weapons.c4s/Script.c
+++ Clinfinity.c4f/Tutorial_Weapons.c4s/Script.c
@@ -22,11 +22,9 @@ func Initialize() {
   CreateObject(_CST, 252,227); //Chest
   
   var Bird=CreateObject(BIRD, 50, 50, 1); //BÖRD!
-  var AIClonk=CreateObject(CLNK,50,50);
+  /*var AIClonk=CreateObject(CLNK,50,50);
   MakeCrewMember(AIClonk,1);
-  Enter(Bird, AIClonk);
-  
-  CreateObject(MUSK,50,50,0);
+  Enter(Bird, AIClonk);*/
   
   //remove all objects for wheat field
   for(var obj in FindObjects(Find_InRect(424, 288, 388, 83))) obj->RemoveObject();
@@ -48,8 +46,8 @@ func InitializePlayer(int plr) {
 	  CreateMatSys(plr);
     // fill with material
     var msys = GetMatSys(plr);
-    msys->DoFill(10, METL);
-    MatSysDoFill(1000,0,STEM);
+    msys->DoFill(100, METL);
+    MatSysDoFill(2000,0,STEM);
 }
 
 func Script1() {
@@ -60,19 +58,41 @@ func Script5() {
 	TutorialMessage("Willkommen zurück!");
 }
 
-func Script10() {
-  TutorialMessage("Begebe dich nun auf den Schießstand. Denk dran: Wenn du irgendwo dranhängst, kannst du mittels Doppel [Hoch] dich nach oben ziehen!");
-  SetArrow(239, 210);
+func Script15() {
+  TutorialMessage("Begebe dich nun auf den Schießstand und nimm die Muskete aus der Kiste! Denk dran: Wenn du an etwas dünnem hangelst, kannst du dich mittels Doppel [Hoch] nach oben ziehen!");
+  CreateContents(MUSK, FindObject(_CST));
+  Sound("Ding");
+  SetArrow(245, 210);
 }
 
-func Script40() {
-	if(!FindObject(AVTR, 160, 50 , 30, 20)) {
-		goto(39);
+func Script30() {
+	if(!FindContents(MUSK, FindObject(AVTR))) {
+		goto(29);
 		return;
 	}
 	SavePosition();
-	TutorialMessage("$TutNice$");
-	Sound("Applause");
+	TutorialMessage("Gut. Mit Werfen kannst du die Muskete anlegen und schießen. Dabei verbraucht sie etwas Dampf und pro Munitionspaket ein Metall!");
 	RemoveArrow();
 }
 
+func Script50() {
+  TutorialMessage("Der Balken über dir zeigt die Stärke des Schusses an, der rote Pfeil zeigt an, dass getroffene Lebewesen bei einem Treffer geschleudert werden. Schieß nun den Vogel ab, er ist böse!");
+}
+
+func Script52() {
+  if(!FindObject(DBRD)) {
+    goto(51);
+    return;
+  }
+  TutorialMessage("Der Vogel ist tot!");
+  Sound("Applause");
+}
+
+func Script60(){
+  TutorialMessage("Lege nun die Muskete wieder in die Kiste und Schnapp dir den Dampfgranatwerfer!");
+}
+
+func Script70(){
+  TutorialMessage("Bonk.");
+  GameOver(1);
+}
--- Clinfinity.c4f/Tutorial_Weapons.c4s/Weed.c4d/Script.c
+++
@@ -1,8 +0,0 @@
-/*-- Neues Objekt --*/
-
-#strict
-
-func Initialize() {
-   
-  return(1);
-}
