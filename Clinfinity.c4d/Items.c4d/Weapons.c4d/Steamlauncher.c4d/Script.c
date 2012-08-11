c8e4766a5bf01c48bd1beda43840d5bd2cde166f
Nachtschatten <ChyH@gmx.net>
2012-08-11 02:50:41 +0200

    Steam launcher, work in progress.

--- Clinfinity.c4d/Items.c4d/Weapons.c4d/Steamlauncher.c4d/Script.c
+++ Clinfinity.c4d/Items.c4d/Weapons.c4d/Steamlauncher.c4d/Script.c
@@ -1,16 +1,127 @@
-/*-- Steamlauncher --*/
+/*	Script: Steam launcher
+	A long-range explosive weapon. */
 
 #strict 2
 
 #include L_SS
 
+static const SGLR_MinDamage = 5;
+static const SGLR_MaxDamage = 20;
+static const SGLR_DamageDeviation = 3;
 
-/* Objekt ist ein Gewehr */
-public func IsRifle () {
+/*	Function: MaxFill
+	Returns the clip size of the steam launcher. */
+public func MaxFill() { return 1; }
+private func FillPicture() { return 0; }
+
+/*	Section: Events */
+
+/*	Function: Initialize
+	Called when the steam launcher is created.
+	It is created with a full magazine. */
+protected func Initialize() {
+	DoFill(MaxFill());
+}
+
+/*	Function: Entrance
+	Event call when entering an object.
+	If the steam launcher is collected by a crew member who is already carrying another steam launcher,
+	their ammunition is combined and the collected steam launcher is removed.
+
+	Parameters:
+	into	- The object that the steam launcher entered. */
+protected func Entrance(object into) {
+	if((into->GetOCF() & OCF_CrewMember) == 0) return;
+	var steamlauncher = FindObject2(Find_Container(into), Find_ID(GetID()), Find_Exclude(this));
+	if(steamlauncher) {
+		steamlauncher->DoFill(GetFill());
+		RemoveObject();
+	}
+}
+
+/*	Function: Load
+	Called by a crew member after its loading animation has finished. */
+public func Load() {
+	if(MatSysDoTeamFill(-1, Contained()->GetOwner(), METL)) {
+		DoFill(MaxFill());
+	}
+}
+
+public func StartLoading() {
+	Sound("SteamlauncherReload");
+}
+
+public func StartAiming() {
+	Sound("MusketDeploy");
+}
+
+
+public func Abort() {/* :'( */}
+
+/*	Section: Controls */
+
+public func CanLoad() {
+	return !IsFull() && MatSysGetTeamFill(Contained()->GetOwner(), METL) >= 1;
+}
+
+public func Fire(object pClonk, int iAngle) {
+	// Coold own
+	if(GetEffect("ReloadRifle", this))
+		return;
+	
+	// Ammo in clip?
+	if(!GetFill()) {
+		Sound("MusketEmpty");
+		// Try to reload
+		pClonk->LoadRifle();
+		return;
+	}
+	// Enough steam?
+	var owner = pClonk->GetOwner();
+	if(MatSysGetTeamFill(owner, STEM) < 5)
+		return;
+	MatSysDoTeamFill(-5, owner, STEM);
+	DoFill(-1);
+	
+    var pObj, pObj2, iX, iY, iR, iXDir, iYDir, iRDir, iDir, iPhase;
+
+	var ammo = CreateContents(CSHO);
+
+    // Austrittsparameter
+    iDir = GetDir(pClonk) * 2 - 1;
+    iPhase = GetPhase(pClonk);
+    //iX = +Sin(iPhase * 19, 14) * iDir;
+    //iY = -Cos(iPhase * 19, 14) - 2;
+    iR = iAngle * iDir + 90;
+    iXDir = (Sin(iAngle, 22) + 1) * iDir;
+    iYDir = -Cos(iAngle, 22);
+    iRDir = 0;
+
+    SetOwner(GetOwner(pClonk), ammo);
+
+    Exit(ammo, AbsX(iX + GetX(pClonk)), AbsY(iY + GetY(pClonk)), iR, iXDir, iYDir, iRDir);
+    ammo->Launch(-1, CalcDamage(), ChargeKnockback());
+
+    // Muzzle flash
+    // hax, weil die Animation nicht genauen Winkeln entspricht und der Partikel seltsam verdreht wird
+    if(iDir == DIR_Left) {
+        if(Inside(iPhase, 0, 1)) iX = +Sin(iPhase * 15, 15) * iDir + 5;
+        else    iX = +Sin(iPhase * 15, 15) * iDir;
+    } else  iX = +Sin(iPhase * 14, 15) * iDir;
+    if(Inside(iPhase, 0, 7)) iY = -Cos(iPhase * 15, 14) - 6;
+    else  iY = -Cos(iPhase * 15, 14) - 3;
+    CreateParticle("MuzzleFlash", AbsX(iX + GetX(pClonk)), AbsY(iY + GetY(pClonk)), iXDir, iYDir, 35, RGBa(255, 255, 255, 0), pClonk);
+
+    Sound("SteamlauncherShoot*", 0, pClonk);
+    Smoke(iX, iY, 2);
+    Smoke(iX, iY + Random(2), 3);
+    CreateParticle("Casing", AbsX(iX / 2 + GetX(pClonk)), AbsY(iY / 2 + GetY(pClonk)), -iDir * RandomX(1, 5), -RandomX(3, 7), 15, RGBa(250, 140, 80, 0));
+    AddEffect("ReloadRifle", this, 101, 30);
+
     return 1;
 }
 
-/* Objekt wurde ausgewählt */
-public func Selection () {
-    Sound("RevolverDraw");
-}
+
+public func IsWeapon()	{ return 1; }
+public func IsRifle()	{ return 1; }
+public func Selection()	{ Sound("RevolverDraw"); }
