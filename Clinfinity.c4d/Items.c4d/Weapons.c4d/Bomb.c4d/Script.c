cac43ba22ebe6169af3802875ebb182d46862769
Nachtschatten <ChyH@gmx.net>
2012-08-11 23:16:42 +0200

    Made steam grenade bounce exactly once.

--- Clinfinity.c4d/Items.c4d/Weapons.c4d/Bomb.c4d/Script.c
+++ Clinfinity.c4d/Items.c4d/Weapons.c4d/Bomb.c4d/Script.c
@@ -1,8 +1,64 @@
-/*-- Neues Objekt --*/
+/*	Script: Steam grenade
+*/
 
-#strict
+#strict 2
 
-func Initialize() {
-   
-  return(1);
+local isBounced;
+
+public func Launch() {
+	SetAction("Active");
+	Sound("steam_exhaust", false, this, 20, 0, 1);
+}
+
+protected func LeakSteam() {
+	Smoke(0, -3, 5);
+}
+
+/*	Section: Events */
+
+protected func Initialize() {
+	isBounced = false;
+}
+
+/*	Function: Departure
+	If a crew member throws the grenade while standing, it activates.
+
+	Parameters:
+	from	- The object the grenade departed from. */
+protected func Departure(object from) {
+	if((from->GetOCF() & OCF_CrewMember) != 0 && from->GetAction() == "Throw") {
+		Launch();
+	}
+}
+
+protected func ContactTop() {
+	Bounce(GetXDir(), -GetYDir());
+}
+
+protected func ContactRight() {
+	Bounce(-GetXDir() / 3, GetYDir());
+}
+
+protected func ContactBottom() {
+	Bounce(GetXDir(), -GetYDir() / 3);
+}
+
+protected func ContactLeft() {
+	Bounce(-GetXDir() / 3, GetYDir());
+}
+
+private func Bounce(int xSpeed, int ySpeed) {
+	if(GetAction() == "Active") {
+		if(!isBounced) {
+			isBounced = true;
+			ScheduleCall(this, "DoBounce", 1, 0, xSpeed, ySpeed);
+		} else {
+			// BOOM!
+		}
+	}
+}
+
+public func DoBounce(int xSpeed, int ySpeed) {
+	SetXDir(xSpeed);
+	SetYDir(ySpeed);
 }
