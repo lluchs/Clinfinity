6dae9b217d470067aa9e5551d5c995c1c571b555
Nachtschatten <ChyH@gmx.net>
2012-08-17 00:53:14 +0200

    Added documentation for grenade steam.

--- Clinfinity.c4d/Items.c4d/Weapons.c4d/Bomb.c4d/BombSteam.c4d/Script.c
+++ Clinfinity.c4d/Items.c4d/Weapons.c4d/Bomb.c4d/BombSteam.c4d/Script.c
@@ -1,14 +1,36 @@
-/*	Script: BombSteam
-	Uses a geometric series to calculate the expansion! */
+/*	Script: Grenade steam
+	Visual steam shockwave effect for the <Steam grenade>.
+	Its animation is divided into two phases: Expansion and fuming.
+
+	Expansion:
+	* A steam cloud moves from the point where it is created to an end point.
+	* Initially, it moves fast but slows down continuously, until its speed is zero at the end point.
+	* Its speed is calculated using a <geometric series at http://en.wikipedia.org/wiki/Geometric_series>.
+
+	When creating a shockwave with several steam clouds,
+	the movement in this phase leads to the visual impression that the steam is stopped by air drag.
+
+	Fuming:
+	* The steam cloud starts moving upwards.
+	* It accelerates until it reaches a randomly chosen maximum speed.
+	* At the same time, it quickly fades out until it disappears completely. */
 
 #strict 2
 
+/*	Constants: Expansion phase
+	BSTE_ExpansionNumerator		- Numerator used in the geometric series.
+	BSTE_ExpansionDenominator	- Denominator used in the geometric series.
+	BSTE_ExpansionFrames		- Length of the expansion phase in frames. */
 static const BSTE_ExpansionNumerator = 1023;
 static const BSTE_ExpansionDenominator = 512;
 static const BSTE_ExpansionFrames = 10;
 
 local maxXDistance, maxYDistance, transparency, targetYSpeed;
 
+/*	Function: LaunchSteam
+	Factory method for launching single steam clouds.
+	The coordinates are relative to the calling object in local calls, otherwise global.
+	*Note:* For best results, launch steam using this method. This is not mandatory, though. */
 public func LaunchSteam(int x, int y, int maxRadius) {
 	var steam = CreateObject(BSTE, x, y + 16, NO_OWNER);
 	var direction = Random(360);
