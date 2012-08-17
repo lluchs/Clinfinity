32aab249b40158ee4430656d76e5fb569553e28d
Nachtschatten <ChyH@gmx.net>
2012-08-18 00:07:55 +0200

    Firefly shouldn't fly down if no ground below, as this happens a lot in Clinfinity. Made script adhere to Clinfinity coding standards, too.

--- Clinfinity.c4d/Animals.c4d/Firefly.c4d/Script.c
+++ Clinfinity.c4d/Animals.c4d/Firefly.c4d/Script.c
@@ -1,39 +1,35 @@
-/*-- Firefly --*/
+/*  Script: Firefly
+*/
 
-#strict
+#strict 2
 
 
-private func Flying() { // wird alle 3 Frames aufgerufen
-  var xdir,ydir;
+private func Flying() {
+	var xdir, ydir;
 
-  // nichts tun
-  if(Random(4)) return(1);
-  // Geschwindigkeit zufällig bestimmen
-  xdir=Random(13)-6;
-  ydir=Random(9)-4;
-  if(GBackLiquid(xdir,ydir)) return(SetSpeed());
-  SetSpeed(xdir,ydir);
+	if(Random(4)) return;
+
+	xdir = Random(13) - 6;
+	ydir = Random(9) - 4;
+	if(GBackLiquid(xdir, ydir)) {
+		SetSpeed(0, 0);
+	} else {
+		SetSpeed(xdir,ydir);
+	}
 }
 
-protected func Check() { // wird alle 18Frames aufgerufen
-
-  // Verschüttet oder im Wasser?: Sterben
-  if(GBackSemiSolid()) return(Death());
-	
-	// too high?
-	if(!GBackSemiSolid(0, 30))
-	{
-		if(!Random(2))
-			SetSpeed(RandomX(-10, 10), RandomX(5, 10));
+protected func Check() {
+	// Buried or in water: Instant death
+	if(GBackSemiSolid()) {
+		Death();
+		return;
 	}
-
 }
 
 protected func Initialize() {
-  SetAction("Fly");
-  SetPhase(Random(6));
-  
-  AddLight(60, RGB(255,255,200), this);
+	SetAction("Fly");
+	SetPhase(Random(6));
+	AddLight(60, RGB(255, 255, 200), this);
 }
 
 public func CatchBlow()	{ RemoveObject(); }
