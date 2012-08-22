84ff76cb45d77d1d0694d54bfd9bf109d2507ab2
Nachtfalter <f.graier@gmx.de>
2012-08-22 22:28:57 +0200

    Sawmill basic functionality

--- Clinfinity.c4d/Structures.c4d/Sawmill.c4d/Script.c
+++ Clinfinity.c4d/Structures.c4d/Sawmill.c4d/Script.c
@@ -6,15 +6,27 @@
 #include L_DC
 #include L_SS
 
+static const TRFM_RADIUS = 350; // Radius in px
+
 public func MaxDamage() { return 20; }
 
-protected func Initialize() {
+protected func Initialize(sawmill) {
 	inherited(...);
-	SetAction("GrabWood");
+
+	var tree = FindObject2(Find_Distance(350, 0, 0), Find_ID(TRE1));
+	  
+	if(tree){
+	  tree -> RemoveObject();
+	  SetAction("GrabWood");
+	  } else {
+	  ScheduleCall(sawmill, "Initialize", 300);
+	}
 }
 
 protected func ControlUp() {
-  Message("Platzhalter für Baum-Radius Anzeige");
+  //show Radius
+  for(var i; i < 360; i++)
+	CreateParticle("PSpark", Cos(i, TRFM_RADIUS), Sin(i, TRFM_RADIUS), 0, 0, 70, RGBa(255, 255, 255, 128));
 }
 
 protected func Finish(obj) {
