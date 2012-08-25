044b440ac061b1aaf68183dfe82056cab815dd1a
Nachtschatten <ChyH@gmx.net>
2012-08-25 18:29:15 +0200

    SkyColourModulation.c: Support for drawing layers subtractively.

--- Clinfinity.c4d/System.c4g/SkyColourModulation.c
+++ Clinfinity.c4d/System.c4g/SkyColourModulation.c
@@ -1,7 +1,6 @@
 /*	Script: SkyColourModulation.c
 	Provides individual colour modulation layers for the sky background.
-	The modulations are combined additively.
-	If the transparency of the foreground colour of any layer is not 0, the combined background colours will be used as background colour behind the sky.
+	Layers can be drawn additively or subtractively.
 
 	With this mechanism, you can apply several different modulations without worrying about their combination yourself.
 	For example, a magic spell can simply set the colours of the appropriate layer, without having to consider other effects on the sky, such as night and day, lightning, etc.
@@ -30,42 +29,49 @@
 /* Internal variables */
 static const SkyColourLayerCount = 10;
 
-static SkyColourForegrounds;
-static SkyColourBackgrounds;
+static SkyColours;
+static SkyColoursSubtractive;
 
 /*	Function: SetSkyColourModulation
 	Sets the colour modulation of the specified layer.
 
 	Parameters:
-	foregroundColour	- Primary colour modulation.
-	backgroundColour	- Background colour.
-	layerIndex			- Index of the layer to be set. */
-global func SetSkyColourModulation(int foregroundColour, int backgroundColour, int layerIndex) {
+	colour		- Colour modulation.
+	subtractive	- If _true_, this layer will be drawn subtractively, additively otherwise.
+	layerIndex	- Index of the layer to be set. */
+global func SetSkyColourModulation(int colour, bool subtractive, int layerIndex) {
 	if(!Inside(layerIndex, 0, SkyColourLayerCount - 1))
 		return;
 	CheckSkyColourArrayInitialization();
-	SkyColourForegrounds[layerIndex] = foregroundColour;
-	SkyColourBackgrounds[layerIndex] = backgroundColour;
+	SkyColours[layerIndex]				= colour;
+	SkyColoursSubtractive[layerIndex]	= subtractive;
 
-	var red, green, blue, alpha, backRed, backGreen, backBlue;
-	for(var i = 0; i < SkyColourLayerCount; i++) {
-		red		+= GetRGBValue(SkyColourForegrounds[i], 1);
-		green	+= GetRGBValue(SkyColourForegrounds[i], 2);
-		blue	+= GetRGBValue(SkyColourForegrounds[i], 3);
-		alpha	+= GetRGBValue(SkyColourForegrounds[i], 0);
-
-		backRed		+= GetRGBValue(SkyColourBackgrounds[i], 1);
-		backGreen	+= GetRGBValue(SkyColourBackgrounds[i], 2);
-		backBlue	+= GetRGBValue(SkyColourBackgrounds[i], 3);
+	var red = 255;
+	var green = 255;
+	var blue = 255;
+	var alpha, backRed, backGreen, backBlue;
+	for(var layer = 0; layer < SkyColourLayerCount; ++layer) {
+		var layerColour = GetSkyColourModulation(layer);
+		if(IsSkyColourModulationSubtractive(layer)) {
+			red		-= GetRGBValue(layerColour, 1);
+			green	-= GetRGBValue(layerColour, 2);
+			blue	-= GetRGBValue(layerColour, 3);
+		} else {
+			red		+= GetRGBValue(layerColour, 1);
+			green	+= GetRGBValue(layerColour, 2);
+			blue	+= GetRGBValue(layerColour, 3);
+		}
 	}
-	red		= Min(255, red);
-	green	= Min(255, green);
-	blue	= Min(255, blue);
-	alpha	= Min(255, alpha);
 
-	backRed		= Min(255, backRed);
-	backGreen	= Min(255, backGreen);
-	backBlue	= Min(255, backBlue);
+	backRed		= BoundBy(red - 255, 0, 255);
+	backGreen	= BoundBy(green - 255, 0, 255);
+	backBlue	= BoundBy(blue - 255, 0, 255);
+
+	red		= BoundBy(red, 0, 255);
+	green	= BoundBy(green, 0, 255);
+	blue	= BoundBy(blue, 0, 255);
+
+	alpha = Max(backRed, Max(backGreen, backBlue));
 
 	SetSkyAdjust(RGBa(red, green, blue, alpha), RGB(backRed, backGreen, backBlue));
 }
@@ -74,36 +80,41 @@ global func SetSkyColourModulation(int foregroundColour, int backgroundColour, i
 	Returns the current colour modulation of the specified layer.
 
 	Parameters:
-	getBackground	- If _true_, the function returns the background colour, otherwise the foreground colour.
 	layerIndex		- Index of the layer. */
-global func GetSkyColourModulation(bool getBackground, int layerIndex) {
+global func GetSkyColourModulation(int layerIndex) {
 	if(!Inside(layerIndex, 0, SkyColourLayerCount - 1))
 		return;
 	CheckSkyColourArrayInitialization();
-	if(getBackground) {
-		return SkyColourBackgrounds[layerIndex];
-	} else {
-		return SkyColourForegrounds[layerIndex];
-	}
+	return SkyColours[layerIndex];
+}
+
+/*	Function: IsSkyColourModulationSubtractive
+	Returns whether the specified layer is drawn additively or subtractively.
+
+	Returns:
+	_true_ if subtractive, _false_ if additive. */
+global func IsSkyColourModulationSubtractive(int layerIndex) {
+	if(!Inside(layerIndex, 0, SkyColourLayerCount - 1))
+		return;
+	CheckSkyColourArrayInitialization();
+	return SkyColoursSubtractive[layerIndex];
 }
 
 /*	Function: ResetSkyColourModulation
-	Resets all layers to default values, so the sky is completely without modulation.
-	*/
+	Resets all layers to default values, so the sky is completely without modulation. */
 global func ResetSkyColourModulation() {
 	CheckSkyColourArrayInitialization();
-	for(var i = 1; i < SkyColourLayerCount; i++) {
-		SkyColourForegrounds[i] = RGBa(0, 0, 0, 0);
-		SkyColourBackgrounds[i] = RGB(0, 0, 0);
+	for(var i = 0; i < SkyColourLayerCount; i++) {
+		SkyColours[i] = RGBa(0, 0, 0, 0);
+		SkyColoursSubtractive[i] = false;
 	}
-	SetSkyColourModulation(RGBa(255, 255, 255, 0), RGB(0, 0, 0), 0);
+	SetSkyAdjust(RGBa(255, 255, 255, 0), RGB(0, 0, 0));
 }
 
 /* Internal helper */
 global func CheckSkyColourArrayInitialization() {
-	if(SkyColourForegrounds == 0) {
-		SkyColourForegrounds = CreateArray(SkyColourLayerCount);
-		SkyColourForegrounds[0] = RGBa(255, 255, 255, 0);
-		SkyColourBackgrounds = CreateArray(SkyColourLayerCount);
+	if(SkyColours == 0) {
+		SkyColours = CreateArray(SkyColourLayerCount);
+		SkyColoursSubtractive = CreateArray(SkyColourLayerCount);
 	}
 }
