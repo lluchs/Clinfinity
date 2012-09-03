e3c908c1a262fa6baaf71b8cedc7ac88837c7e8c
Nachtschatten <ChyH@gmx.net>
2012-09-03 21:45:09 +0200

    Bird song environment object adapted to TIME. Also added rooster and owl sound on daybreak/nightfall.

--- Clinfinity.c4d/Enviroment.c4d/BirdSong.c4d/Script.c
+++ Clinfinity.c4d/Enviroment.c4d/BirdSong.c4d/Script.c
@@ -2,27 +2,54 @@
 
 #strict 2
 
-protected func Initialize() {
-    SetAction("Sing");
-	// randomize start
-    SetPhase(Random(20));
-    return 1;
+#include RULE
+
+local tweetCount;
+
+private func Initialized(int ruleTypeCount) {
+    tweetCount = ruleTypeCount;
+
+    if(IsDay()) ScheduleTweets();
+
+    var time = FindObject2(Find_ID(TIME));
+    if(time == 0) return;
+
+    time->AddEventListener(this, "OnDay");
+    time->AddEventListener(this, "OnNightfall");
+    time->AddAlarmListener(this, time->GetDaybreakTime() + TIME_TwilightLength / 4);
+    time->AddAlarmListener(this, time->GetNightfallTime() + TIME_TwilightLength * 3 / 4);
+}
+
+private func ScheduleTweets() {
+    for(var i = 0; i < tweetCount; ++i) {
+        // randomize start
+        ScheduleCall(0, "Singing", 25 - Random(20));
+    }
+}
+
+public func OnAlarm(object clock, int time) {
+    if(IsDaybreak()) Sound("Rooster*");
+    else Sound("Owl");
+}
+
+public func OnDay(object clock) {
+    ScheduleTweets();
+}
+
+public func OnNightfall(object clock) {
+    ClearScheduleCall(this, "Singing");
 }
 
 private func Singing() {
-    // Is night? Sleep!
-    if(IsNight())
-        return;
-
     if(!Random(8)) {
 		// check if there are some trees left
-        if(ObjectCount2(Find_Category(C4D_StaticBack), Find_Func("IsTree")) >= 10){
+        if(ObjectCount2(Find_Category(C4D_StaticBack), Find_Func("IsTree")) >= 10) {
             //Sound("BirdSong*", 1);
             var sound = Random(15)+1;
             Sound(Format("BirdSong%d", sound));
             //Log("%d", sound);
-            }
-		else
+        } else
 			Sound("Crow*", 1);
     }
+    ScheduleCall(this, "Singing", 25);
 }
