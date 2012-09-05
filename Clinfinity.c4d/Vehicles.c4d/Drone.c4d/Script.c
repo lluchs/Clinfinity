a95109ee5b569c159db74a8b46aa1c37992c5776
Nachtschatten <ChyH@gmx.net>
2012-09-06 01:34:07 +0200

    Drone: Try to find a place to drill.

--- Clinfinity.c4d/Vehicles.c4d/Drone.c4d/Script.c
+++ Clinfinity.c4d/Vehicles.c4d/Drone.c4d/Script.c
@@ -11,19 +11,74 @@ static const DRNE_DrillRadius = 10;
 local targetX, targetY;
 
 protected func Initialize() {
-	targetX = GetX();
-	targetY = GetY();
-	SetAction("Fly");
+	Stop();
 }
 
 public func MoveTo(int x, int y) {
 	targetX = x;
 	targetY = y;
+	SetAction("Fly");
 }
+
+public func Stop() {
+	targetX = GetX();
+	targetY = GetY();
+	SetSpeed(0);
+	if(GetAction() != "FlyIdle") {
+		SetAction("FlyIdle");
+	}
+}
+
+public func Drill() {
+	SetAction("Drill");
+}
+
+protected func DecideAction() {
+	if(FindObject2(Find_ID(GetID()), Find_AtPoint(0, 0), Find_Exclude(this)) != 0) {
+		if(FindDrillingPosition()) {
+			SetAction("Fly");
+		}
+	} else if(GetMaterial(0, 0) != Material("Rock")) {
+		if(FindDrillingPosition()) {
+			SetAction("Fly");
+		}
+	} else if(GetMaterial(0, 0) == Material("Rock")) {
+		Drill();
+	}
+	/*
+
+	Drones should:
+		- Get a destination from the quarry and fly there
+		- Be able to find new rock to drill nearby
+		- Detect: If at the destination there already is another drone drilling, find some other place
+		- If full: Fly back to the quarry and deposit collected rock
+	*/
+}
+
+// 
+private func FindDrillingPosition() {
+	for(var radius = DRNE_DrillRadius * 2; radius < DRNE_DrillRadius * 10; radius += DRNE_DrillRadius) {
+		var searchAngle = Random(360);
+		for(var j = 0; j < 360; j += 10) {
+			var searchX = Sin(searchAngle + j, radius);
+			var searchY = -Cos(searchAngle + j, radius);
+			if(GetMaterial(searchX, searchY) == Material("Rock")) {
+				targetX = searchX + GetX();
+				targetY = searchY + GetY();
+				return true;
+			}
+		}
+	}
+	return false;
+}
+
+
+
+/* Action-Calls */
 
 protected func Flying() {
 	if(Distance(GetX(), GetY(), targetX, targetY) < DRNE_StopDistance) {
-		SetSpeed(0);
+		Stop();
 		return;
 	}
 	var angle = Angle(GetX(), GetY(), targetX, targetY);
