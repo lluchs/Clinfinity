cbbb9878b9da4de49de21295757930aeb309c112
Nachtschatten <ChyH@gmx.net>
2012-09-06 05:17:55 +0200

    Quarry gets five mining drones on construction.

--- Clinfinity.c4d/Structures.c4d/Quarry.c4d/Script.c
+++ Clinfinity.c4d/Structures.c4d/Quarry.c4d/Script.c
@@ -4,6 +4,13 @@
 
 #include STBO
 
+protected func Initialize() {
+	for(var i = 0; i < 5; ++i) {
+		DRNE->CreateDrone(GetX(), GetY(), GetOwner(), this);
+	}
+	return _inherited(...);
+}
+
 protected func Quarry() {
 	Sound("Quarry");
 }
