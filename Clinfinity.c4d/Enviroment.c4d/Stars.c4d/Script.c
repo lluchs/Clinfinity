9eb7e850784d7dac89b4f30cc9836ab3633c75e1
Nachtschatten <ChyH@gmx.net>
2012-09-14 14:27:59 +0200

    Stars: Show the Big Dipper at night!

--- Clinfinity.c4d/Enviroment.c4d/Stars.c4d/Script.c
+++ Clinfinity.c4d/Enviroment.c4d/Stars.c4d/Script.c
@@ -4,6 +4,8 @@
 
 #include RULE
 
+local constellations;
+
 private func Initialized(int ruleTypeCount) {
 	var time = FindObject2(Find_ID(TIME));
 	if(time == 0) {
@@ -13,6 +15,10 @@ private func Initialized(int ruleTypeCount) {
 	SetPosition(0, 0);
 	time->AddEventListener(this, "OnNightfall");
 	time->AddAlarmListener(this, time->GetDaybreakTime() - 255 * TIME_SecondsPerFrame);
+
+	// Constellations
+	constellations = [];
+	PushBack(CreateObject(NSBD, Random(LandscapeWidth()), Random(LandscapeHeight()), NO_OWNER), constellations);
 
 	var maxStarsCount = (LandscapeWidth() * LandscapeHeight() * ruleTypeCount) / 20000;
 	for(var i = 0; i < maxStarsCount; ++i) {
@@ -24,11 +30,17 @@ public func OnNightfall() {
 	for(var star in FindObjects(Find_ID(STAR))) {
 		ScheduleCall(0, "FadeStarIn", RandomX(TIME_TwilightLength * 9 / 10 / TIME_SecondsPerFrame, TIME_TwilightLength / TIME_SecondsPerFrame), 0, star);
 	}
+	for(var constellation in constellations) {
+		ScheduleCall(0, "FadeStarIn", RandomX(TIME_TwilightLength * 9 / 10 / TIME_SecondsPerFrame, TIME_TwilightLength / TIME_SecondsPerFrame), 0, constellation);
+	}
 }
 
 public func OnAlarm(object clock, int time) {
 	for(var star in FindObjects(Find_ID(STAR))) {
 		ScheduleCall(0, "FadeStarOut", RandomX(1, TIME_TwilightLength / 10 / TIME_SecondsPerFrame), 0, star);
+	}
+	for(var constellation in constellations) {
+		ScheduleCall(0, "FadeStarOut", RandomX(1, TIME_TwilightLength / 10 / TIME_SecondsPerFrame), 0, constellation);
 	}
 }
 
