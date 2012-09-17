131c1f6a2aa052b1ae7367c24399a86f6c75a4fa
Nachtschatten <ChyH@gmx.net>
2012-09-17 13:11:34 +0200

    Added search radius tooltip for sawmill controls.

--- Clinfinity.c4d/Structures.c4d/Sawmill.c4d/Script.c
+++ Clinfinity.c4d/Structures.c4d/Sawmill.c4d/Script.c
@@ -21,6 +21,7 @@ protected func Initialize(sawmill) {
 }
 
 protected func ControlUp() {
+	[$TxtShowSearchRadius$]
 	//show Radius
 	for(var i; i < 360; i++)
 		CreateParticle("PSpark", Cos(i, SAWM_RADIUS), Sin(i, SAWM_RADIUS), 0, 0, 70, RGBa(255, 255, 255, 128));
