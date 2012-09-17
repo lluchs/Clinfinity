140b4e9e03249832a5aecc8ee399b61f58da0a7f
Nachtfalter <f.graier@gmx.de>
2012-09-17 19:10:02 +0200

    Increase respawn area, closes #32

--- Clinfinity.c4f/DUEL_CrumblingIslands.c4s/Script.c
+++ Clinfinity.c4f/DUEL_CrumblingIslands.c4s/Script.c
@@ -51,8 +51,8 @@ func Initialize() {
 	  PlaceVines();
 	  
 	  // Island Respawn
-  	PeriodicIslandRespawn(1993, 140, 750, 330, 100); // left 'home' island
-  	PeriodicIslandRespawn(2007, 1620, 730, 310, 100); // right 'home' island
+  	PeriodicIslandRespawn(1993, 140, 750, 330, 250); // left 'home' island
+  	PeriodicIslandRespawn(2007, 1620, 730, 310, 250); // right 'home' island
 }
 
 func InitializePlayer(int plr) {
