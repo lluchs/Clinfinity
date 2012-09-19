6a7c2d4dcbdc4092c0b18859df10a6c47add803d
Nachtschatten <ChyH@gmx.net>
2012-09-19 17:03:32 +0200

    Testing wielding animations for shield and sword.

--- Clinfinity.c4d/Items.c4d/Weapons.c4d/Shield.c4d/Script.c
+++ Clinfinity.c4d/Items.c4d/Weapons.c4d/Shield.c4d/Script.c
@@ -3,3 +3,18 @@
 protected func Hit() {
 	Sound("MetalHit*");
 }
+
+public func IsMeleeWeapon()		{ return true; }
+public func GetWieldDirection()	{ return AVMW_WieldUp; }
+
+public func WieldStart() {
+	Message("SHIT WieldStart");
+}
+
+public func WieldAbort() {
+	Message("SHIT WieldAbort");
+}
+
+public func WieldEnd() {
+	Message("SHIT WieldEnd");
+}
--- Clinfinity.c4d/Items.c4d/Weapons.c4d/Sword.c4d/Script.c
+++ Clinfinity.c4d/Items.c4d/Weapons.c4d/Sword.c4d/Script.c
@@ -1,1 +1,31 @@
 #strict 2
+
+public func IsMeleeWeapon()		{ return true; }
+public func GetWieldDirection()	{ return AVMW_WieldDown; }
+
+public func WieldStart() {
+	Message("Sword WieldStart");
+	// Add defence effect
+}
+
+public func WieldAbort() {
+	Message("Sword WieldAbort");
+	// RemoveEffect(); remove defence effect
+}
+
+public func WieldEnd() {
+	/*for(var clonk in FindObjects(...)) {
+		clonk->DoEnergy(-9000);
+	}*/
+	Message("Sword WieldEnd");
+}
+
+/*
+public func FxWhatever() {
+	for(var incoming in FindObjects(...)) {
+		if(correctAngleTo(incoming)) {
+			FlingAway(incoming);
+		}
+	}
+}
+*/
