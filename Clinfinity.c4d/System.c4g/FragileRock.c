a2ccc39322abc4212fce4198e2723f4e44aa93aa
Nachtschatten <ChyH@gmx.net>
2012-09-27 02:25:40 +0200

    Rocks burst when hitting the ground too hard. Closes #58.

---
+++ Clinfinity.c4d/System.c4g/FragileRock.c
@@ -0,0 +1,15 @@
+#strict 2
+
+#appendto ROCK
+
+protected func Hit() {
+	_inherited(...);
+	if(GetOCF() & OCF_HitSpeed4) {
+		var colourIndex = Random(3);
+		var red =	GetMaterialColor(Material("Rock"), colourIndex, 0);
+		var green =	GetMaterialColor(Material("Rock"), colourIndex, 1);
+		var blue =	GetMaterialColor(Material("Rock"), colourIndex, 2);
+		CastParticles("MatSpark", RandomX(3, 6), RandomX(20, 30), 0, 0, 40, 60, RGBa(red, green, blue, 50), RGBa(red, green, blue, 50));
+		RemoveObject();
+	}
+}
