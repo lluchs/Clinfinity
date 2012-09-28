d8bc60f74cfd6fe584716da15648e52fe2b3f9d2
Nachtfalter <f.graier@gmx.de>
2012-09-28 16:53:25 +0200

    Fragile Rock Sound

--- Clinfinity.c4d/System.c4g/FragileRock.c
+++ Clinfinity.c4d/System.c4g/FragileRock.c
@@ -10,6 +10,7 @@ protected func Hit() {
 		var green =	GetMaterialColor(Material("Rock"), colourIndex, 1);
 		var blue =	GetMaterialColor(Material("Rock"), colourIndex, 2);
 		CastParticles("MatSpark", RandomX(3, 6), RandomX(20, 30), 0, 0, 40, 60, RGBa(red, green, blue, 50), RGBa(red, green, blue, 50));
+		Sound("Rock_burst");
 		RemoveObject();
 	}
 }
