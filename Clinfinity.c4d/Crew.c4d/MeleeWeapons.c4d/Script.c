9d02563e5035ab9aebb6895e6af46d17f46b5f68
Nachtschatten <ChyH@gmx.net>
2012-09-30 02:24:41 +0200

    Wrote proper documentation for melee weapons ability.

--- Clinfinity.c4d/Crew.c4d/MeleeWeapons.c4d/Script.c
+++ Clinfinity.c4d/Crew.c4d/MeleeWeapons.c4d/Script.c
@@ -1,14 +1,58 @@
 /*	Script: Melee weapons
-	Functionality for using melee weapons. */
+	Functionality for using melee weapons.
+
+	Melee weapons are short range weapons wielded with both hands.
+	Therefore, only one melee weapon can be used at a time.
+	Additionally, some other actions, such as throwing, using other types of weapons, etc, are also disabled while wielding.
+	
+	Wielding:
+	Wielding a melee weapon consists of two phases:
+	- The actual wielding
+	- A cool down, which is optional
+
+	For both phases, several weapon movement directions are available.
+	See <Wield directions> and <Cool down directions> for details.
+
+	Animation:
+	The aviator provides the appropriate actions for the wielding and cool down phases,
+	with the weapon automatically rendered into the aviator's hands.
+	A weapon can determine its appearance in the aviator's hands by setting an action while being wielded.
+	Otherwise, the weapon's default graphics is used.
+
+	Events:
+	To make animation, weapon effects, etc. as easy as possible,
+	various functions are invoked on the weapon object at different times.
+	See <Calls to weapons> for details.
+
+	Melee weapon objects:
+	Melee weapon objects must implement the following functions: 
+	- IsMeleeWeapon()		- Return _true_ here.
+	- GetWieldDirection()	- Specifies the wield direction.
+							  Return one of the defined <Wield directions> here.
+	- GetStartAngle()		- Angle in which the weapon starts when wielding.
+	- GetCentralAngle()		- Angle and direction to wield the weapon.
+
+	Beyond minimal functionality,
+	the following functions may be implemented:
+	- GetCoolDownDirection()	- Specifies the movement during the cool down phase.
+								  Implement this function to provide cool down for a weapon.
+								  Return one of the defined <Cool down directions> here. */
 
 #strict 2
 
+/*	Constants: Wield directions
+	AVMW_WieldUp	- Direction: Wield weapon upwards.
+	AVMW_WieldDown	- Direction: Wield weapon downwards. */
 static const AVMW_WieldUp	= 1;
 static const AVMW_WieldDown	= 2;
 
+/*	Constants: Cool down directions
+	AVMW_WieldReverse	- Direction: Wield weapon back to the initial angle.
+	AVMW_WieldHold		- Direction: Hold weapon in its final position for a short time span. */
 static const AVMW_WieldReverse	= 3;
 static const AVMW_WieldHold		= 4;
 
+/* Constants for internal use only, therefore not publicly documented. */
 static const AVMW_WieldDownHandX = 0;
 static const AVMW_WieldDownHandY = -5;
 static const AVMW_WieldUpHandX = 0;
@@ -20,6 +64,11 @@ static const AVMW_WieldUpShoulderY		= 1;
 local activeMeleeWeapon, startAngle, angularSpeed, endAngle;
 local isRegularActionSwitch;
 
+/*	Function: ReadyToWield
+	Returns if the Clonk is currently ready to start wielding a melee weapon.
+
+	Returns:
+	_true_ if ready, _false_ otherwise. */
 public func ReadyToWield() {
 	// Can't use more than one melee weapon
 	if(YOYO->IsYoyoThrownBy(this)) return false;
@@ -31,11 +80,28 @@ private func IsGliding() {
 	return _inherited();
 }
 
+/*	Function: IsWielding
+	Returns if the Clonk is currently wielding a melee weapon.
+	If a weapon is passed as parameter, _IsWielding()_ checks if this weapon is wielded.
+	On the other hand, if no weapon is passed, the function checks if any weapon is wielded.
+
+	Parameters:
+	weapon	- [optional] Weapon to check if it is wielded. May be omitted for checking for any weapon.
+
+	Returns:
+	_true_ if a weapon is wielded, _false_ otherwise. */
 public func IsWielding(object weapon) {
 	if(weapon == 0) return activeMeleeWeapon != 0;
 	else return activeMeleeWeapon == weapon;
 }
 
+/*	Function: GetCurrentWieldData
+	Function to access the current position and angle of the hands while wielding a weapon.
+
+	Parameters:
+	handX		- Horizontal hand coordinate.
+	handY		- Vertical hand coordinate.
+	weaponAngle	- Hand angle. */
 public func GetCurrentWieldData(&handX, &handY, &weaponAngle) {
 	if(activeMeleeWeapon != 0) {
 		var shoulderY;
@@ -227,16 +293,32 @@ private func CallToWeapon(string callName, a) {
 }
 
 /*	Section: Calls to weapons
-	The functions described in this section are called in the weapon object at the appropriate times. */
+	The functions described in this section are invoked on the weapon object at the appropriate times. */
 
+/*	Function: WieldStart
+	Called when a Clonk starts wielding the weapon.
+
+	Parameters:
+	direction	- The direction the Clonk looks in. */
 public func WieldStart(int direction) {}
 
+/*	Function: WieldEnd
+	Called when a Clonk successfully finished wielding the weapon.
+	This function is invoked regardless of a cool down following or not. */
 public func WieldEnd() {}
 
+/*	Function: WieldAbort
+	Called when wielding was aborted before finished. */
 public func WieldAbort() {}
 
+/*	Function: CoolDownStart
+	Called when the cool down phase starts. */
 public func CoolDownStart() {}
 
+/*	Function: CoolDownEnd
+	Called when the cool down phase was successfully finished. */
 public func CoolDownEnd() {}
 
+/*	Function: CoolDownAbort
+	Called when the cool down phase was aborted before finished. */
 public func CoolDownAbort() {}
