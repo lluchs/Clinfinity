33b31a8791eb39d641f458409c773333eebbfee5
Nachtfalter <f.graier@gmx.de>
2012-11-25 05:49:41 +0100

    Update heart

--- Clinfinity.c4f/DM_ClockworkHeart.c4s/Heart.c4d/Script.c
+++ Clinfinity.c4f/DM_ClockworkHeart.c4s/Heart.c4d/Script.c
@@ -2,7 +2,16 @@
 
 #strict 2
 
-func Initialize() {
-  SetAction("Open");
+func Initialize(obj) {
+	ScheduleCall(obj, "OpenHeart", 500);
   return(1);
 }
+
+protected func OpenHeart(){
+	SetAction("Open");
+	}
+
+protected func Breathe(){
+	Log("Test");
+	CreateParticle("Smoke", 30, -18, 0, 0, 50, RGBa(255, 255, 255, 0));
+  }
