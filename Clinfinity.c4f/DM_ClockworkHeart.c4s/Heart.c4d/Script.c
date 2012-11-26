3df2c595b361c591effa83c940720afda81c6fa1
Nachtfalter <f.graier@gmx.de>
2012-11-26 19:21:50 +0100

    WIP Heart

--- Clinfinity.c4f/DM_ClockworkHeart.c4s/Heart.c4d/Script.c
+++ Clinfinity.c4f/DM_ClockworkHeart.c4s/Heart.c4d/Script.c
@@ -12,7 +12,12 @@ protected func OpenHeart() {
 	SetAction("Open");
 	}
 
-protected func Breathe(){
+protected func Breathe(obj){
+  ScheduleCall(obj, "MakeNoise", 55);
 	CastParticles("Smoke", 500, 200, 80, 95);
 	CreateObject(WIPF, 80, 95);
+}
+
+protected func MakeNoise(){
+	Sound("Heart_Valve");
 }
