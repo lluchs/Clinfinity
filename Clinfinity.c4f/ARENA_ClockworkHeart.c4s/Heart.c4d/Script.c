fde8a808666bb6b78a38529044a7be384bfb231d
Nachtfalter <f.graier@gmx.de>
2012-11-27 16:24:11 +0100

    Heart Wip

--- Clinfinity.c4f/ARENA_ClockworkHeart.c4s/Heart.c4d/Script.c
+++ Clinfinity.c4f/ARENA_ClockworkHeart.c4s/Heart.c4d/Script.c
@@ -14,10 +14,22 @@ protected func OpenHeart() {
 
 protected func Breathe(obj){
   ScheduleCall(obj, "MakeNoise", 55);
-	CastParticles("Smoke", 500, 200, 80, 95);
-	CreateObject(WIPF, 80, 95);
+  ScheduleCall(obj, "MakeSmoke", 50);
 }
 
-protected func MakeNoise(){
+protected func MakeNoise() {
 	Sound("Heart_Valve");
+}
+
+protected func MakeSmoke() {
+	CastParticles("Smoke", RandomX(10, 20), 195, 60, RandomX(65, 75), 50, 70, RGBa(200, 200, 200, 10), RGBa(255, 255, 255, 200));
+	CastParticles("Smoke", RandomX(30, 60), 195, 70, 105, 70, 200, RGBa(200, 200, 200, 10), RGBa(255, 255, 255, 200));
+	CastParticles("Smoke", RandomX(20, 30), 195, 75, 120, 400, 600, RGBa(200, 200, 200, 10), RGBa(255, 255, 255, 240));
+}
+
+protected func MakeSmoke2() {	//(TimerCall)
+ Sound("Steam*.wav");
+ CastParticles("Smoke", RandomX(20, 30), RandomX(30, 60), -114, 82, 50, 70, RGBa(200, 200, 200, 10), RGBa(200, 200, 200, 190));
+ CastParticles("Smoke", RandomX(20, 30), RandomX(30, 60), -103, 71, 50, 70, RGBa(200, 200, 200, 10), RGBa(200, 200, 200, 190));
+ CastParticles("Smoke", RandomX(20, 30), RandomX(30, 60), -89, 60, 50, 70, RGBa(200, 200, 200, 10), RGBa(200, 200, 200, 190));
 }
