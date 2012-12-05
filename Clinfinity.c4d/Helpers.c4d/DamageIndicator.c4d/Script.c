7d563a1971e7a9be8a9c5651097642323e980bc5
Nachtschatten <ChyH@gmx.net>
2012-12-05 17:59:11 +0100

    Damage indicator fades out now. Closes #99.

--- Clinfinity.c4d/Helpers.c4d/DamageIndicator.c4d/Script.c
+++ Clinfinity.c4d/Helpers.c4d/DamageIndicator.c4d/Script.c
@@ -1,10 +1,28 @@
 #strict 2
 
+static const DIND_Duration = 35;
+
+local amount, alphaEase;
+
 public func IndicateDamage(object damagedObject, int amount) {
 	if(damagedObject == 0) return;
+
 	var x = damagedObject->GetX() - GetX();
 	var y = damagedObject->GetY() - GetY();
 	var indicator = CreateObject(DIND, x, y, NO_OWNER);
-	indicator->CustomMessage(Format("@-%d", amount), indicator, NO_OWNER, 0, 0, RGB(255, 0, 0));
 	Fling(indicator, RandomX(-2, 2), RandomX(-2, 1));
+
+	indicator->LocalN("alphaEase") = CreateEaseFunction("quad-in-out", DIND_Duration);
+	indicator->LocalN("amount") = amount;
+	AddEffect("AlphaFade", indicator, 1, 1, indicator);
+}
+
+protected func FxAlphaFadeTimer(object target, int effectNumber, int effectTime) {
+	if(effectTime > DIND_Duration) {
+		RemoveObject();
+		return FX_Execute_Kill;
+	}
+	var alpha = ChangeRange(EvalEase(alphaEase, effectTime), 0, DIND_Duration, 0, 255);
+	CustomMessage(Format("@-%d", amount), this, NO_OWNER, 0, 0, RGBa(255, 0, 0, alpha));
+	return FX_OK;
 }
