f3e9433a64398bd88e752d36d61ad11df2a98c77
Nachtschatten <ChyH@gmx.net>
2012-12-07 17:25:35 +0100

    Added spawner that reacts on lightning strikes.

---
+++ Clinfinity.c4f/ARENA_ClockworkHeart.c4s/InvisibleSpawner.c4d/Script.c
@@ -0,0 +1,21 @@
+
+#strict 2
+
+public func LightningStrike() {
+	var randomId = SelectRandomId();
+	var randomObject = CreateObject(randomId, 0, 0, NO_OWNER);
+	if(randomId == XBOW) {
+		CreateContents(ARWP, randomObject);
+	}
+}
+
+private func SelectRandomId() {
+	var r = 6;//Random(7);
+	if(!r)   return FLNT;
+	if(!--r) return SFLN;
+	if(!--r) return EFLN;
+	if(!--r) return BOMB;
+	if(!--r) return MUSK;
+	if(!--r) return SGLR;
+	if(!--r) return XBOW;
+}
