1e59ce3e6eb20d7a07c87b8c23d7f551df7a5dae
Nachtschatten <ChyH@gmx.net>
2012-12-07 17:46:30 +0100

    Spawner: Don't spawn more than one object when multiple lighting strikes occur almost at the same time.

--- Clinfinity.c4f/ARENA_ClockworkHeart.c4s/InvisibleSpawner.c4d/Script.c
+++ Clinfinity.c4f/ARENA_ClockworkHeart.c4s/InvisibleSpawner.c4d/Script.c
@@ -2,11 +2,15 @@
 #strict 2
 
 public func LightningStrike() {
+	if(GetEffect("DelaySpawning", this)) return;
+
 	var randomId = SelectRandomId();
 	var randomObject = CreateObject(randomId, 0, 0, NO_OWNER);
 	if(randomId == XBOW) {
 		CreateContents(ARWP, randomObject);
 	}
+	// Don't spawn more than one object when multiple lighting strikes occur almost at the same time
+	AddEffect("DelaySpawning", this, 101, 5);
 }
 
 private func SelectRandomId() {
