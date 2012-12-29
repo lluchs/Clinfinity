d4eac39d14257fbc1f42c735583ce42fd666482d
Luchs <Luchs@euirc.eu>
2012-12-30 00:04:52 +0100

    Fix zero call involving Yo-Yos flying out

--- Clinfinity.c4f/ARENA_ClockworkHeart.c4s/System.c4g/DeathEmptyInventory.c
+++ Clinfinity.c4f/ARENA_ClockworkHeart.c4s/System.c4g/DeathEmptyInventory.c
@@ -6,7 +6,9 @@
 protected func Death(int killedBy) {
 	var contents = FindObjects(Find_Owner(GetOwner()), Find_Exclude(this));
 	for(var item in contents) {
-		item->RemoveObject();
+		// The item might actually already be gone.
+		if(item)
+			item->RemoveObject();
 	}
 	return _inherited(killedBy);
 }
